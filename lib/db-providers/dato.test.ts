045ee03f97c9b12e825bc46d929a208ff0aefd2e
Ross Ragsdale <ross@pointblankdev.com>
2023-08-02 04:31:46 -0500

    :white_check_mark: Update test

--- lib/db-providers/dato.test.ts
+++ lib/db-providers/dato.test.ts
@@ -2,7 +2,7 @@ import { createUser, getUserById, updateUserWithWallet } from "./dato";
 
 describe('getUserById function', () => {
     it('should return a user for a valid id', async () => {
-        const id = '158986255'; // replace with a valid user ID from your system
+        const id = '158987222'; // replace with a valid user ID from your system
 
         // Note: The actual structure and content of the returned user will depend on your specific data model
         // You may need to modify the below expectation to match the actual structure returned by your client
@@ -36,7 +36,7 @@ describe('getUserById function', () => {
     });
 
     it('should update a user with an email', async () => {
-        const userId = '158986255';
+        const userId = '158987222';
         const walletId = '158986996';
         const wallet = await updateUserWithWallet(userId, walletId)
 
