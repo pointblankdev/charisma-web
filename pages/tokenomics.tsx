7587863ac4d9103bcd6a0de1dccc2a28a5804f6c
Ross Ragsdale <ross@pointblankdev.com>
2023-08-09 15:55:51 -0500

    :sparkles: Add dynamic unique holder tracking via faucet monitoring

--- pages/tokenomics.tsx
+++ pages/tokenomics.tsx
@@ -11,13 +11,23 @@ import {
   CardHeader,
   CardTitle,
 } from "@components/ui/card"
+import { useEffect } from 'react';
+import { GetStaticProps } from 'next';
+import { callReadOnlyFunction } from '@stacks/transactions';
+import { StacksMainnet } from "@stacks/network";
+import { accountsApi } from '@lib/stacks-api';
 
 
-export default function Tokenomics() {
+export default function Tokenomics({ data }: Props) {
   const meta = {
     title: 'Charisma | Tokenomics',
     description: META_DESCRIPTION
   };
+
+  const totalSupplyWithDecimals = data.totalSupply / 1000000
+  const dripAmountPerDayWithDecimals = (data.dripAmount / 1000000 * 1440).toFixed(6)
+  const uniqueAddresses = data.uniqueAddresses
+  const uniqueAddressesIncreaseSinceLastMonth = `${data.uniqueAddresses.length - 1}`
 
   return (
     <Page meta={meta} fullViewport>
@@ -30,12 +40,12 @@ export default function Tokenomics() {
                 <CardTitle className="text-sm font-medium">
                   Unique Wallet Addresses
                 </CardTitle>
-                <div className="text-2xl font-bold">16</div>
+                <div className="text-2xl font-bold">{uniqueAddresses.length}</div>
               </CardHeader>
               <CardContent>
 
                 <p className="text-xs font-semibold text-muted-foreground">
-                  +1500% from last month
+                  {`+${uniqueAddressesIncreaseSinceLastMonth}00% from last month`}
                 </p>
               </CardContent>
             </Card>
@@ -44,12 +54,12 @@ export default function Tokenomics() {
                 <CardTitle className="text-sm font-medium">
                   Total Supply
                 </CardTitle>
-                <div className="text-2xl font-bold">0.003758</div>
+                <div className="text-2xl font-bold">{totalSupplyWithDecimals}</div>
               </CardHeader>
               <CardContent>
 
                 <p className="text-xs font-semibold text-muted-foreground">
-                  ~0.000144 tokens minted per day
+                  ~{dripAmountPerDayWithDecimals} tokens minted per day
                 </p>
               </CardContent>
             </Card>
@@ -93,3 +103,76 @@ export default function Tokenomics() {
   );
 }
 
+async function fetchAllClaims() {
+  let offset = 0;
+  const limit = 50;
+  const uniqueWallets: Set<string> = new Set(); // This will keep track of unique wallets
+
+  while (true) {
+    const f: any = await accountsApi.getAccountTransactionsWithTransfers({
+      principal: 'SP2ZNGJ85ENDY6QRHQ5P2D4FXKGZWCKTB2T0Z55KS.dme005-token-faucet-v0',
+      limit: limit,
+      offset: offset
+    });
+
+    if (!f.results.length) {
+      break; // exit the loop if there are no more results
+    }
+
+    f.results.forEach((r: any) => {
+      // console.log(r.tx.sender_address);
+      if (r.tx.contract_call?.function_name === 'claim' && r.tx.tx_result.repr === '(ok true)') {
+        uniqueWallets.add(r.tx.sender_address);
+      }
+    });
+
+    offset += limit; // increment the offset for the next page
+  }
+
+  console.log(`Total unique wallets that claimed the token: ${uniqueWallets.size}`);
+  return uniqueWallets;
+}
+
+
+type Props = {
+  data: any;
+};
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+
+
+  const r: any = await callReadOnlyFunction({
+    network: new StacksMainnet(),
+    contractAddress: "SP2D5BGGJ956A635JG7CJQ59FTRFRB0893514EZPJ",
+    contractName: "dme000-governance-token",
+    functionName: "get-total-supply",
+    functionArgs: [],
+    senderAddress: 'SP2D5BGGJ956A635JG7CJQ59FTRFRB0893514EZPJ'
+  })
+
+
+  const d: any = await callReadOnlyFunction({
+    network: new StacksMainnet(),
+    contractAddress: "SP2ZNGJ85ENDY6QRHQ5P2D4FXKGZWCKTB2T0Z55KS",
+    contractName: "dme005-token-faucet-v0",
+    functionName: "get-drip-amount",
+    functionArgs: [],
+    senderAddress: 'SP2D5BGGJ956A635JG7CJQ59FTRFRB0893514EZPJ'
+  })
+
+
+
+  const uniqueAddresses = await fetchAllClaims()
+
+
+  return {
+    props: {
+      data: {
+        totalSupply: Number(r.value.value),
+        dripAmount: Number(d.value.value),
+        uniqueAddresses: [...uniqueAddresses]
+      }
+    },
+    revalidate: 60
+  };
+};
