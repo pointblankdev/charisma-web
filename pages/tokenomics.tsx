cec9f596db1d6def202d08b53d625da26a61940e
Ross Ragsdale <ross@pointblankdev.com>
2023-08-09 06:03:27 -0500

    :art: Update text styles

--- pages/tokenomics.tsx
+++ pages/tokenomics.tsx
@@ -61,30 +61,30 @@ export default function Tokenomics() {
             </Card> */}
           </div>
 
-          <p className="mb-4">
+          <p className="mb-4 font-light">
             The Charisma Token operates within a unique economic model designed for transparency, controlled distribution, and a self-sustaining ecosystem.
           </p>
 
-          <ol className="list-decimal pl-5">
+          <ol className="list-decimal pl-5 font-extralight">
             <li className="mb-3">
-              <b>Token Faucet Mechanism</b>: At the heart of the DAO's tokenomics is the Charisma Token Faucet. This system drips or mints Charisma tokens over a set period. The rate, known as the "drip amount," defines the number of tokens released per blockchain block. This drip mechanism ensures a steady and predictable token supply growth, giving users and investors a clear view of issuance.
+              <b className='font-semibold'>Token Faucet Mechanism</b>: At the heart of the DAO's tokenomics is the Charisma Token Faucet. This system drips or mints Charisma tokens over a set period. The rate, known as the "drip amount," defines the number of tokens released per blockchain block. This drip mechanism ensures a steady and predictable token supply growth, giving users and investors a clear view of issuance.
             </li>
 
             <li className="mb-3">
-              <b>Claiming Tokens</b>: Users have the opportunity to claim tokens from the faucet. The amount claimable is a product of the drip rate and the number of blocks since the last claim. This system not only promotes user interaction but also ensures a decentralized distribution of tokens over time.
+              <b className='font-semibold'>Claiming Tokens</b>: Users have the opportunity to claim tokens from the faucet. The amount claimable is a product of the drip rate and the number of blocks since the last claim. This system not only promotes user interaction but also ensures a decentralized distribution of tokens over time.
             </li>
 
             <li className="mb-3">
-              <b>Controlled Issuance</b>: While users can claim tokens, the drip amount or rate of token issuance can only be adjusted by the DAO or its authorized extensions. This provision preserves the integrity of the tokenomics, ensuring no unauthorized or unpredictable supply surges.
+              <b className='font-semibold'>Controlled Issuance</b>: While users can claim tokens, the drip amount or rate of token issuance can only be adjusted by the DAO or its authorized extensions. This provision preserves the integrity of the tokenomics, ensuring no unauthorized or unpredictable supply surges.
             </li>
 
             <li className="mb-3">
-              <b>Transparency & Security</b>: All token issuance and transactions are recorded on the blockchain, offering a transparent audit trail. Moreover, measures are in place to prevent any abuse, like unauthorized changes to the drip amount or claims when insufficient balance is available.
+              <b className='font-semibold'>Transparency & Security</b>: All token issuance and transactions are recorded on the blockchain, offering a transparent audit trail. Moreover, measures are in place to prevent any abuse, like unauthorized changes to the drip amount or claims when insufficient balance is available.
             </li>
           </ol>
 
-          <p className="mt-4">
-            In essence, the Charisma Token DAO emphasizes a stable growth model, transparency, and user-centric distribution, aiming to build trust, value, and a strong community foundation.
+          <p className="mt-4 font-light">
+            In essence, this approach emphasizes a stable growth model, transparency, and user-centric distribution, aiming to build trust, value, and a strong community foundation.
           </p>
 
         </div>
