83ef3b8f6ccbfac5523b8b2bd379e14564e01876
Ross Ragsdale <ross@pointblankdev.com>
2023-08-16 02:54:15 -0500

    :art: Update

--- pages/quests/index.tsx
+++ pages/quests/index.tsx
@@ -163,8 +163,8 @@ export const data = [
       metric: '0/1'
     }],
     description: [
-      `In a realm where the pulse of Bitcoin illuminates every shadow, ALEX Lab stands as a beacon of innovation and wonder. Nestled at the intersection of magic and logic, this sanctuary is often cloaked in mystery, known only to the select few with a thirst for alchemical pursuits.`,
-      ` A siren's song resonates through the alleys and marketplaces, a melody imbued with the power of two tokens becoming one. A fusion, they say, more profound than mere combination. It's the alchemy of liquidity, a dance of elements led by the grand alchemists of ALEX Lab.`,
+      `In a realm where the pulse of bitcoin illuminates every shadow, one guild stands as a beacon of innovation and wonder. Nestled at the intersection of magic and logic, this sanctuary is often cloaked in mystery, known only to the select few with a thirst for alchemical pursuits.`,
+      ` Whispers of a mystic fusion echo through the alleys and marketplaces, like the gentle release of vapors from an alchemist's vial, signaling the melding of two tokens into one. A fusion, they say, more profound than mere combination. It's the alchemy of liquidity, a dance of elements led by the grand alchemists of ALEX Lab.`,
       ` As you navigate the bustling streets, a sigil marked with an X – the emblem of ALEX Lab – catches your gaze. An enigmatic figure, draped in robes that shimmer like the night sky, beckons. "Do you seek the secrets of the Liquidity Pair Token?" she inquires with a knowing smile, offering you a chance to be a part of this transcendent fusion.`,
     ]
   },
