2cb968756e67583abdd0e2f74f027e5ca647430a
Ross Ragsdale <ross@pointblankdev.com>
2023-08-26 01:22:33 -0500

    :sparkles: Add discord bot endpoint

---
+++ pages/api/interactions.ts
@@ -0,0 +1,31 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+type ErrorResponse = {
+  error: {
+    code: string;
+    message: string;
+  };
+};
+
+export default function chainhooks(
+  req: NextApiRequest,
+  res: NextApiResponse<any | ErrorResponse>
+) {
+  if (req.method !== 'POST') {
+    return res.status(501).json({
+      error: {
+        code: 'method_unknown',
+        message: 'This endpoint only responds to POST'
+      }
+    });
+  }
+
+  try {
+    console.log(req.body)
+
+  } catch (error: any) {
+    console.error(error.message)
+  }
+
+  return res.status(200).json({});
+}
