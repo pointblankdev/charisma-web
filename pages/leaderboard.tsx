8467a25e7f97d04cdd561309e358536f3c8d97d1
Tolujoh-n <tolujohnofficial@gmail.com>
2023-09-04 21:46:40 +0100

    leaderboard updated

--- pages/leaderboard.tsx
+++ pages/leaderboard.tsx
@@ -10,121 +10,162 @@ import Link from 'next/link';
 import { fetchAllContractTransactions, getProposals, updateVoteData } from '@lib/stacks-api';
 import dmlogo from '@public/dm-logo.png';
 
-type Props = {
-  data: any[];
+// SearchBar component
+const SearchBar = ({ onSearch }) => {
+  const [searchText, setSearchText] = useState('');
+
+  const handleSearch = () => {
+    onSearch(searchText);
+  };
+
+  return (
+    <div className="search-bar">
+      <input
+        type="text"
+        placeholder="Search for an address"
+        value={searchText}
+        onChange={e => setSearchText(e.target.value)}
+        style={{
+          borderRadius: '5px',
+          borderColor: 'red',
+          backgroundColor: 'transparent',
+          padding: '5px'
+        }}
+      />
+      <button onClick={handleSearch} style={{ borderColor: 'red', backgroundColor: 'transparent' }}>
+        <i className="fas fa-search" style={{ color: 'red' }}></i>
+      </button>
+    </div>
+  );
 };
 
-interface LeaderboardEntry {
-  num: number;
-  image: string;
-  address: string;
-  powerScore: number;
-  questsCompleted: number;
-  link: string;
-}
+// FilterBar component
+const FilterBar = ({ activeFilter, onFilterChange }) => {
+  const filters = ['7 days', '30 days', 'All-time'];
 
+  return (
+    <div className="filter-bar">
+      {filters.map(filter => (
+        <button
+          key={filter}
+          onClick={() => onFilterChange(filter)}
+          className={activeFilter === filter ? 'active' : ''}
+          style={{
+            borderRadius: '5px',
+            borderColor: 'red',
+            backgroundColor: activeFilter === filter ? 'rgb(65, 62, 62)' : 'black',
+            color: activeFilter === filter ? 'white' : 'white'
+          }}
+        >
+          {filter}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+// Leaderboard component
 export default function Leaderboard() {
   const meta = {
     title: 'Charisma | Leaderboard',
     description: META_DESCRIPTION
   };
-  const [data, setData] = useState<LeaderboardEntry[]>([]);
+  const [data, setData] = useState([]); // Store your data here
+  const [searchedText, setSearchedText] = useState('');
+  const [activeFilter, setActiveFilter] = useState('All-time');
 
+  // Simulated API call to fetch data (replace with actual API call)
   useEffect(() => {
-    // API endpoint
-    const apiUrl = 'https://api';
-
-    // Fetch data from the API
-    fetch(apiUrl)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
+    // Replace this with your actual API call logic
+    // For demonstration, we're using dummy data here
+    const fetchData = async () => {
+      try {
+        const response = await fetch('your-api-endpoint-here');
+        if (response.ok) {
+          const result = await response.json();
+          setData(result);
+        } else {
+          console.error('Failed to fetch data');
         }
-        return response.json();
-      })
-      .then(json => setData(json))
-      .catch(error => console.error('Error fetching data:', error));
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
+
+    fetchData();
   }, []);
+
+  const handleSearch = text => {
+    setSearchedText(text);
+  };
+
+  const handleFilterChange = filter => {
+    setActiveFilter(filter);
+  };
+
+  // Filter and search data here based on 'searchedText' and 'activeFilter'
+  const filteredData = data.filter(entry =>
+    entry.address.toLowerCase().includes(searchedText.toLowerCase())
+  );
 
   return (
     <Page meta={meta} fullViewport>
       <SkipNavContent />
       <Layout>
-        <div className="m-2 sm:container sm:mx-auto sm:py-10">
-          <div className="flex justify-between items-end">
-            <h1 className="text-xl text-left mt-8 mb-2 text-gray-200">Leaderboard</h1>
-            <Link href="/governance/guide">
-              <Button variant={'link'} className="my-2 px-0">
-                DAO Contributer Guide 📕
-              </Button>
-            </Link>
-          </div>
-          <div className="leaderboard">
-            <table>
-              <thead>
-                <tr>
-                  <th>Rank</th>
-                  <th>Name</th>
-                  <th>Power Score</th>
-                  <th>Quests Completed</th>
-                  <th></th>
+        <div id="lead">
+        <div className="flex justify-between items-end">
+          <SearchBar onSearch={handleSearch} />
+          <FilterBar activeFilter={activeFilter} onFilterChange={handleFilterChange} />
+        </div>
+        <div className="leaderboard">
+          <table>
+            <thead>
+              <tr>
+                <th>Rank</th>
+                <th>Name</th>
+                <th>Power Score</th>
+                <th>Quests Completed</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {filteredData.map((entry, index) => (
+                <tr key={index}>
+                  <td>{entry.num}</td>
+                  <td>
+                    <div className="name-wrapper">
+                      <img src={entry.image} alt="Avatar" />
+                      <span>{entry.address}</span>
+                      <button
+                        onClick={() => {
+                          // Handle copy address logic here
+                          const textarea = document.createElement('textarea');
+                          textarea.value = entry.address;
+                          document.body.appendChild(textarea);
+                          textarea.select();
+                          document.execCommand('copy');
+                          document.body.removeChild(textarea);
+                        }}
+                        className="copy-icon"
+                      >
+                        Copy
+                      </button>
+                    </div>
+                  </td>
+                  <td>{entry.powerScore}</td>
+                  <td>{entry.questsCompleted}</td>
+                  <td>
+                    <a href={entry.link}>
+                      <i className="fas fa-external-link-alt"></i>
+                    </a>
+                  </td>
                 </tr>
-              </thead>
-              <tbody>
-                {data.map((entry, index) => (
-                  <tr key={index}>
-                    <td>{entry.num}</td>
-                    <td>
-                      <div className="name-wrapper">
-                        <img src={entry.image} alt="Avatar" />
-                        <span>{entry.address}</span>
-                        <button
-                          onClick={() => {
-                            // Handle copy address logic here
-                            const textarea = document.createElement('textarea');
-                            textarea.value = entry.address;
-                            document.body.appendChild(textarea);
-                            textarea.select();
-                            document.execCommand('copy');
-                            document.body.removeChild(textarea);
-                          }}
-                          className="copy-icon"
-                        >
-                          Copy
-                        </button>
-                      </div>
-                    </td>
-                    <td>{entry.powerScore}</td>
-                    <td>{entry.questsCompleted}</td>
-                    <td>
-                      <a href={entry.link}>
-                        <i className="fas fa-external-link-alt"></i>
-                      </a>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
+              ))}
+            </tbody>
+          </table>
+        </div>
         </div>
       </Layout>
     </Page>
   );
 }
-
-export const getStaticProps: GetStaticProps<Props> = async () => {
-  const proposals = await getProposals();
-
-  const transactions = await fetchAllContractTransactions(
-    'SP2D5BGGJ956A635JG7CJQ59FTRFRB0893514EZPJ.dme001-proposal-voting'
-  );
-
-  const updatedProposals = updateVoteData(proposals, transactions);
-
-  return {
-    props: {
-      data: updatedProposals
-    },
-    revalidate: 60
-  };
-};
