ee3b1280527a9fa56b6a8ddbb1c2e2858a7eddbf
Ross Ragsdale <ross@pointblankdev.com>
2023-09-19 05:28:03 -0500

    :art: Update

--- components/settings/sidebar-nav.tsx
+++ components/settings/sidebar-nav.tsx
@@ -31,7 +31,7 @@ export function SidebarNav({ className, items, ...props }: SidebarNavProps) {
           className={cn(
             buttonVariants({ variant: "ghost" }),
             pathname === item.href
-              ? "bg-accent-foreground hover:bg-muted text-black"
+              ? "bg-accent-foreground hover:bg-muted"
               : "hover:bg-muted",
             "justify-start"
           )}
