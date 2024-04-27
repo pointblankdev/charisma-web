251436f57e9d15d993dd709e7b6aab55a96c4002
Ross Ragsdale <rossragsdale@gmail.com>
2024-04-27 10:19:33 -0500

    :art: Update

---
+++ components/stake/stake-odin.tsx
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from "react";
+import { useConnect } from "@stacks/connect-react";
+import { StacksMainnet } from "@stacks/network";
+import {
+  AnchorMode,
+  Pc,
+  PostConditionMode,
+  uintCV,
+} from "@stacks/transactions";
+import ConnectWallet, { userSession } from "../stacks-session/connect";
+import { Button } from "@components/ui/button";
+import { toInteger } from "lodash";
+
+interface StakeOdinButtonProps {
+  tokens: string;
+}
+
+const StakeOdinButton: React.FC<StakeOdinButtonProps> = ({ tokens }) => {
+  const { doContractCall } = useConnect();
+
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  const tokens6Dec = Number(tokens) * 1000000
+
+  function stake() {
+    const sender = userSession.loadUserData().profile.stxAddress.mainnet;
+    doContractCall({
+      network: new StacksMainnet(),
+      anchorMode: AnchorMode.Any,
+      contractAddress: "SP2ZNGJ85ENDY6QRHQ5P2D4FXKGZWCKTB2T0Z55KS",
+      contractName: "liquid-staked-odin",
+      functionName: "stake",
+      functionArgs: [uintCV(tokens6Dec)],
+      postConditionMode: PostConditionMode.Deny,
+      postConditions: [
+        Pc.principal(sender)
+          .willSendEq(tokens6Dec)
+          .ft(
+            "SP2C1WREHGM75C7TGFAEJPFKTFTEGZKF6DFT6E2GE.odin-tkn",
+            "odin"
+          ),
+      ],
+      onFinish: (data) => {
+        console.log("onFinish:", data);
+      },
+      onCancel: () => {
+        console.log("onCancel:", "Transaction was canceled");
+      },
+    });
+  }
+
+  if (!mounted || !userSession.isUserSignedIn()) {
+    return <ConnectWallet />;
+  }
+
+  return (
+    <Button
+      className="text-md w-full hover:bg-[#ffffffee] hover:text-primary"
+      onClick={stake}
+      disabled={toInteger(tokens) <= 0}
+    >
+      Stake {tokens && toInteger(tokens) > 0 ? tokens : 0} ODIN
+    </Button>
+  );
+};
+
+export default StakeOdinButton;
---
+++ components/stake/unstake-odin.tsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from "react";
+import { useConnect } from "@stacks/connect-react";
+import { StacksMainnet } from "@stacks/network";
+import {
+  AnchorMode,
+  Pc,
+  PostConditionMode,
+  uintCV,
+} from "@stacks/transactions";
+import ConnectWallet, { userSession } from "../stacks-session/connect";
+import { Button } from "@components/ui/button";
+import { toInteger } from "lodash";
+
+interface UnstakeOdinButtonProps {
+  tokens: string;
+}
+
+const UnstakeOdinButton: React.FC<UnstakeOdinButtonProps> = ({ tokens }) => {
+  const { doContractCall } = useConnect();
+
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => { setMounted(true) }, []);
+
+  const tokens6Dec = Number(tokens) * 1000000
+
+  function unstake() {
+    doContractCall({
+      network: new StacksMainnet(),
+      anchorMode: AnchorMode.Any,
+      contractAddress: "SP2ZNGJ85ENDY6QRHQ5P2D4FXKGZWCKTB2T0Z55KS",
+      contractName: "liquid-staked-odin",
+      functionName: "unstake",
+      functionArgs: [uintCV(tokens6Dec)],
+      postConditionMode: PostConditionMode.Allow,
+      postConditions: [],
+      onFinish: (data) => {
+        console.log("onFinish:", data);
+      },
+      onCancel: () => {
+        console.log("onCancel:", "Transaction was canceled");
+      },
+    });
+  }
+
+  if (!mounted || !userSession.isUserSignedIn()) {
+    return <ConnectWallet />;
+  }
+
+  return (
+    <Button
+      variant={'ghost'}
+      className='text-md w-full hover:bg-[#ffffffee] hover:text-primary'
+      onClick={unstake}
+      disabled={toInteger(tokens) <= 0}>
+      Unstake {tokens && toInteger(tokens) > 0 ? tokens : 0} sODIN
+    </Button>
+  );
+};
+
+export default UnstakeOdinButton;
