325d0fa74f62ff3d65873468b903bc0249c97bb6
Ross Ragsdale <ross@pointblankdev.com>
2024-06-27 00:30:35 -0500

    :art: Update

--- pages/deploy.tsx
+++ pages/deploy.tsx
@@ -179,6 +179,26 @@ export default function ContractEditor({ quest }: any) {
 	(ok (var-get token-uri))
 )
 
+;; --- Utility functions
+
+(define-public (send-many (recipients (list 200 { to: principal, amount: uint, memo: (optional (buff 34)) })))
+  (fold check-err (map send-token recipients) (ok true))
+)
+
+(define-private (check-err (result (response bool uint)) (prior (response bool uint)))
+  (match prior ok-value result err-value (err err-value))
+)
+
+(define-private (send-token (recipient { to: principal, amount: uint, memo: (optional (buff 34)) }))
+  (send-token-with-memo (get amount recipient) (get to recipient) (get memo recipient))
+)
+
+(define-private (send-token-with-memo (amount uint) (to principal) (memo (optional (buff 34))))
+  (let ((transferOk (try! (transfer amount tx-sender to memo))))
+    (ok transferOk)
+  )
+)
+
 ;; --- Extension callback
 
 (define-public (callback (sender principal) (memo (buff 34)))
