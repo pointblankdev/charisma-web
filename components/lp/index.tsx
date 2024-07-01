4c5f0485e1fafc05703441eb72bf7c6d27b2d234
Ross Ragsdale <ross@pointblankdev.com>
2024-06-30 23:12:44 -0500

    :art: Update

--- components/lp/index.tsx
+++ components/lp/index.tsx
@@ -1,26 +1,18 @@
-
-import { useState } from 'react';
-import { WalletBalancesContext, WalletBalances } from '@lib/hooks/use-wallet-balances';
 import Layout from '../layout';
 import Hero from './hero';
 import LearnMore from './learn-more';
 import styleUtils from '@components/utils.module.css';
 
-
 export default function LandingPage() {
 
-  const [balances, setBalances] = useState<WalletBalances>({} as any);
-
   return (
-    <WalletBalancesContext.Provider value={{ balances, setBalances }}>
-      <Layout>
-        <div className={styleUtils.container}>
-          <>
-            <Hero />
-            <LearnMore />
-          </>
-        </div>
-      </Layout>
-    </WalletBalancesContext.Provider>
+    <Layout>
+      <div className={styleUtils.container}>
+        <>
+          <Hero />
+          <LearnMore />
+        </>
+      </div>
+    </Layout>
   );
 }
