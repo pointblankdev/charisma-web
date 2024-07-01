52a6a7a1210c31eb6ebc04f41ddcb7c5836e0d7f
Ross Ragsdale <ross@pointblankdev.com>
2024-07-01 08:36:03 -0500

    :art: Update

--- lib/velar-api.ts
+++ lib/velar-api.ts
@@ -2,23 +2,11 @@ const velarApi = {
     tokens: async (symbol = 'all') => {
         const response = await fetch(`https://api.velar.co/tokens/?symbol=${symbol}`)
         const data = await response.json();
-        data.push({
-            symbol: 'CHA',
-            name: 'Charisma',
-            contractAddress: 'SP2D5BGGJ956A635JG7CJQ59FTRFRB0893514EZPJ.dme000-governance-token',
-            price: '2'
-        })
         return data
     },
     tickers: async () => {
         const response = await fetch('https://api.velar.co/tickers')
         const data = await response.json();
-        data.push({
-            ticker_id: 'SP1Y5YSTAHZ88XYK1VPDH24GY0HPX5J4JECTMY4A1.wstx_SP2D5BGGJ956A635JG7CJQ59FTRFRB0893514EZPJ.dme000-governance-token',
-            base_currency: 'SP1Y5YSTAHZ88XYK1VPDH24GY0HPX5J4JECTMY4A1.wstx',
-            target_currency: 'SP2D5BGGJ956A635JG7CJQ59FTRFRB0893514EZPJ.dme000-governance-token',
-            last_price: 2,
-        })
         return data
     }
 };
