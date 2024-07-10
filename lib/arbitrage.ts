94aea047573ec48779421c7561069aa8cbbb15de
Ross Ragsdale <ross@pointblankdev.com>
2024-07-10 05:27:15 -0500

    :art: Update

--- lib/arbitrage.ts
+++ lib/arbitrage.ts
@@ -29,10 +29,17 @@ export async function runAll() {
     const mempoolTxs3 = await getArbitrageTxsFromMempool('SP2ZNGJ85ENDY6QRHQ5P2D4FXKGZWCKTB2T0Z55KS.icc-arb-2');
     const mempoolTxs4 = await getArbitrageTxsFromMempool('SP2ZNGJ85ENDY6QRHQ5P2D4FXKGZWCKTB2T0Z55KS.arbitrage-w-s-sw-w-zf');
 
-    const mempoolTxs = [...mempoolTxs2, ...mempoolTxs3, ...mempoolTxs4]
+    let mempoolTxs = [...mempoolTxs2, ...mempoolTxs3, ...mempoolTxs4]
 
     console.log({ arbitrageJobs: config.jobs, gasFee: config.gasFee })
     console.log({ mempoolTxs: mempoolTxs.map((tx: any) => tx) })
+
+    // filter out jobs in mempool with receipt_time > 2 hours
+    const now = new Date().getTime();
+    const twoHoursAgo = now - 2 * 60 * 60 * 1000;
+    mempoolTxs = mempoolTxs.filter((tx: any) => {
+        return Number(tx.receipt_time) > twoHoursAgo;
+    });
 
     // get highest none in mempool
     let highestNonce = mempoolTxs.reduce((acc: number, tx: any) => {
