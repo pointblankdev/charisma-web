add56c69c0ef943268018e12a2b664033279153f
firerum <ademuyiwaadewuyi@gmail.com>
2024-07-11 20:48:56 +0100

    arbitrage-dashboard project launch

--- pages/arbitrage/dashboard.tsx
+++ pages/arbitrage/dashboard.tsx
@@ -4,22 +4,21 @@ import { META_DESCRIPTION } from '@lib/constants';
 import { SkipNavContent } from '@reach/skip-nav';
 
 export default function Dashboard() {
+  const meta = {
+    title: 'Charisma | Arbitrage Dashboard',
+    description: META_DESCRIPTION,
+    image: '/liquid-charisma.png'
+  };
 
-    const meta = {
-        title: 'Charisma | Arbitrage Dashboard',
-        description: META_DESCRIPTION,
-        image: '/liquid-charisma.png'
-    };
-
-    return (
-        <Page meta={meta} fullViewport>
-            <SkipNavContent />
-            <Layout>
-                <div className="m-2 sm:container sm:mx-auto sm:py-10">
-                    <h1>Arbitrage Dashboard</h1>
-
-                </div>
-            </Layout>
-        </Page>
-    );
+  return (
+    <Page meta={meta} fullViewport>
+      <SkipNavContent />
+      <Layout>
+        <div className="m-2 sm:container sm:mx-auto sm:py-10">
+          <h1>Arbitrage Dashboard</h1>
+          {/* Hey Rozar, do we have a template UI for the arbitrage dashboard? I can't seem to find any */}
+        </div>
+      </Layout>
+    </Page>
+  );
 }
