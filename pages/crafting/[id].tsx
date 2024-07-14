b5f8d2dabfecc5adec562835344380c041a7fab2
Ross Ragsdale <ross@pointblankdev.com>
2024-07-13 19:11:10 -0500

    :art: Update

--- pages/crafting/[id].tsx
+++ pages/crafting/[id].tsx
@@ -294,8 +294,8 @@ export default function IndexDetailPage({ data }: Props) {
               </Link>
               <div className='flex flex-col justify-end space-y-1'>
                 {isApples && claimableAmount > 0 && <div className='animate-pulse text-center text-sm'>{farmers} farmers are working...</div>}
-                {isApples && <Button size={'sm'} className={`z-30 ${claimableAmount === 0 && "animate-pulse"}`} onClick={claimableAmount > 0 ? harvest : store}>
-                  {farmers > 0 && (claimableAmount > 0 ? `Harvest ${millify(claimableAmount)} Fuji Apples` : 'Assign farmers to work')}
+                {isApples && farmers > 0 && <Button size={'sm'} className={`z-30 ${claimableAmount === 0 && "animate-pulse"}`} onClick={claimableAmount > 0 ? harvest : store}>
+                  {claimableAmount > 0 ? `Harvest ${millify(claimableAmount)} Fuji Apples` : 'Assign farmers to work'}
                 </Button>}
                 {descriptionVisible && (absValMin !== maxPossibleIndex) && (
                   <LiquidityControls
