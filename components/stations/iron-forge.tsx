b6e8c3f0e362f1b4bacd2d264a6c7d8ac9a7829d
Ross Ragsdale <ross@pointblankdev.com>
2024-07-16 18:15:40 -0500

    :art: Update

--- components/stations/iron-forge.tsx
+++ components/stations/iron-forge.tsx
@@ -157,7 +157,7 @@ export default function IronForgeCard({ data }: any) {
                         Epoch Progress: {epochProgress}%
                     </div>
                     <div className="z-30 bg-background border border-primary/40 rounded-full px-2">
-                        Supply Utilization: {supplyUtilization}%
+                        Ingots Forged This Epoc: {supplyUtilization}%
                     </div>
                 </div>
             </CardHeader>
