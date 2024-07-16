325f7ce1a2513040e9fb5f3ddb1d0ce302372af2
Ross Ragsdale <ross@pointblankdev.com>
2024-07-15 23:25:59 -0500

    :art: Update

--- lib/user-api.test.ts
+++ lib/user-api.test.ts
@@ -2,26 +2,26 @@ import { getContractMetadata, setContractMetadata } from "./user-api"
 
 describe('User API', () => {
     it('should get metadata', async () => {
-        const response = await getContractMetadata('SP2ZNGJ85ENDY6QRHQ5P2D4FXKGZWCKTB2T0Z55KS.mr-president-pepe')
+        const response = await getContractMetadata('SP2ZNGJ85ENDY6QRHQ5P2D4FXKGZWCKTB2T0Z55KS.presidental-pepe')
         const result = await response.json()
         console.log(result)
     })
 
     it('should set metadata', async () => {
-        const response = await setContractMetadata('SP2ZNGJ85ENDY6QRHQ5P2D4FXKGZWCKTB2T0Z55KS.president-pepe', {
+        const response = await setContractMetadata('SP2ZNGJ85ENDY6QRHQ5P2D4FXKGZWCKTB2T0Z55KS.presidental-pepe', {
             "name": "President Pepe",
             "description": "An index token composed of sPEPE and sCHA at a fixed 10k:1 ratio.",
             "image": "https://www.charisma.rocks/indexes/presidential-pepe-logo.jpg",
             "background": "https://www.charisma.rocks/indexes/presidential-pepe-bg.jpg",
             "symbol": "iPP",
             "ft": "index-token",
-            "weight": "1",
+            "weight": 1,
             "contains": [
                 {
                     "address": "SP2ZNGJ85ENDY6QRHQ5P2D4FXKGZWCKTB2T0Z55KS.liquid-staked-pepe",
                     "symbol": "sPEPE",
                     "ft": "liquid-staked-token",
-                    "weight": 10
+                    "weight": 10000
                 },
                 {
                     "address": "SP2ZNGJ85ENDY6QRHQ5P2D4FXKGZWCKTB2T0Z55KS.liquid-staked-charisma",
