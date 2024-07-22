961814772b95c5a030a4894d8ccd2a0289b64e43
Ross Ragsdale <ross@pointblankdev.com>
2024-07-22 18:40:47 -0500

    :art: Update

---
+++ pages/apps/battle-royale.tsx
@@ -0,0 +1,441 @@
+import { SkipNavContent } from '@reach/skip-nav';
+import Page from '@components/page';
+import { META_DESCRIPTION } from '@lib/constants';
+import Layout from '@components/layout';
+import Image from 'next/image';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle
+} from '@components/ui/card';
+import MintRaven from '@components/mint/raven';
+import { Button } from '@components/ui/button';
+import {
+  blocksApi,
+} from '@lib/stacks-api';
+import { GetStaticProps } from 'next';
+import { useEffect, useLayoutEffect, useState } from 'react';
+import { cn } from '@lib/utils';
+import Link from 'next/link';
+import Typewriter from 'typewriter-effect';
+import { motion } from 'framer-motion';
+import odinsRaven from '@public/odins-raven/img/4.gif';
+import fenrirIcon from '@public/fenrir-icon-2.png';
+import goldEmbers from '@public/quests/gold-embers.gif'
+import charismaticCorgi from '@public/indexes/charismatic-corgi-bg.png'
+import experience from '@public/experience.png'
+import { userSession } from '@components/stacks-session/connect';
+import { useConnect } from '@stacks/connect-react';
+import { AnchorMode, callReadOnlyFunction, cvToJSON, Pc, PostConditionMode, principalCV, uintCV } from '@stacks/transactions';
+import { StacksMainnet } from "@stacks/network";
+import numeral from 'numeral';
+import farmersImg from '@public/creatures/img/farmers.png'
+import blacksmithsImg from '@public/creatures/img/blacksmiths.png'
+import corgiSoldiersImg from '@public/creatures/img/corgi-soldiers.png'
+import alchemistsImg from '@public/creatures/img/alchemists.png'
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogTitle, DialogTrigger } from '@components/ui/dialog';
+import { AlertDialogHeader } from '@components/ui/alert-dialog';
+import energyIcon from '@public/creatures/img/energy.png'
+import fujiApples from '@public/stations/fuji-apples.png'
+import { Progress } from "@components/ui/progress"
+
+export default function BattleRoyale({ data }: Props) {
+  const meta = {
+    title: "Charisma | Battle Royale",
+    description: META_DESCRIPTION,
+    image: '/journey-of-discovery.png'
+  };
+
+  const title = "Battle Royale";
+  const subtitle = 'Bid your energy for a shot at glory and the grand prize.';
+
+  const sender = userSession.isUserSignedIn() && userSession.loadUserData().profile.stxAddress.mainnet
+
+  const [descriptionVisible, setDescriptionVisible] = useState(false);
+  const [currentEpoch, setCurrentEpoch] = useState(0)
+  const [supplyPerEpoch, setSupplyPerEpoch] = useState(0)
+  const [epochProgress, setEpochProgress] = useState(0)
+  const [highestBid, setHighestBid] = useState(0)
+  const [highestBidder, setHighestBidder] = useState('')
+
+
+  useEffect(() => {
+    try {
+      setDescriptionVisible(true);
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+
+
+
+  useEffect(() => {
+    sender && callReadOnlyFunction({
+      network: new StacksMainnet(),
+      contractAddress: "SP2ZNGJ85ENDY6QRHQ5P2D4FXKGZWCKTB2T0Z55KS",
+      contractName: 'battle-royale-v2',
+      functionName: "get-current-epoch",
+      functionArgs: [],
+      senderAddress: sender
+    }).then(response => setCurrentEpoch(Number(cvToJSON(response).value)))
+  }, [sender])
+
+  useEffect(() => {
+    sender && callReadOnlyFunction({
+      network: new StacksMainnet(),
+      contractAddress: "SP2ZNGJ85ENDY6QRHQ5P2D4FXKGZWCKTB2T0Z55KS",
+      contractName: 'battle-royale-v2',
+      functionName: "get-per-epoch",
+      functionArgs: [],
+      senderAddress: sender
+    }).then(response => setSupplyPerEpoch(Number(cvToJSON(response).value)))
+  }, [sender])
+
+  useEffect(() => {
+    sender && callReadOnlyFunction({
+      network: new StacksMainnet(),
+      contractAddress: "SP2ZNGJ85ENDY6QRHQ5P2D4FXKGZWCKTB2T0Z55KS",
+      contractName: 'battle-royale-v2',
+      functionName: "get-epoch-progress",
+      functionArgs: [],
+      senderAddress: sender
+    }).then(response => setEpochProgress(Number(cvToJSON(response).value)))
+  }, [sender])
+
+  useEffect(() => {
+    sender && callReadOnlyFunction({
+      network: new StacksMainnet(),
+      contractAddress: "SP2ZNGJ85ENDY6QRHQ5P2D4FXKGZWCKTB2T0Z55KS",
+      contractName: 'battle-royale-v2',
+      functionName: "get-highest-bid",
+      functionArgs: [uintCV(currentEpoch)],
+      senderAddress: sender
+    }).then(response => setHighestBid(Number(cvToJSON(response).value)))
+  }, [sender, currentEpoch])
+
+  useEffect(() => {
+    sender && callReadOnlyFunction({
+      network: new StacksMainnet(),
+      contractAddress: "SP2ZNGJ85ENDY6QRHQ5P2D4FXKGZWCKTB2T0Z55KS",
+      contractName: 'battle-royale-v2',
+      functionName: "get-highest-bidder",
+      functionArgs: [uintCV(currentEpoch)],
+      senderAddress: sender
+    }).then(response => setHighestBidder(cvToJSON(response).value))
+  }, [sender, currentEpoch])
+
+  const fadeIn = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1 }
+  };
+
+  return (
+    <Page meta={meta} fullViewport>
+      {/* <Image src={goldEmbers} alt="bolt-background-image" layout="fill" objectFit="cover" priority /> */}
+      <SkipNavContent />
+      <Layout>
+        <motion.div
+          initial="hidden"
+          animate="visible"
+          variants={fadeIn}
+          className="m-2 sm:container sm:mx-auto sm:py-10 md:max-w-2xl"
+        >
+          <Card className="min-h-[600px] flex flex-col bg-black text-primary-foreground border-accent-foreground p-0 relative overflow-hidden rounded-md group/card w-full max-w-2xl opacity-[0.99] shadow-black shadow-2xl">
+            <CardHeader className="z-20 p-4">
+              <div className="flex items-center justify-between">
+                <CardTitle className="z-30 text-xl font-semibold">{title}</CardTitle>
+              </div>
+              <CardDescription className="z-30 pb-6 text-md font-fine text-foreground">
+                {subtitle}
+              </CardDescription>
+              <div className='grid grid-cols-2'>
+
+                <div className="z-20">
+                  <CardTitle className="z-30 mt-2 text-xl font-semibold">Grand Prize</CardTitle>
+                  <CardDescription className="z-30 mb-4 text-sm font-fine text-foreground">
+                    The top bidder will recieve:
+                  </CardDescription>
+                  <div className="grid grid-cols-3 gap-4">
+                    <div className="relative">
+                      <Image
+                        alt="Fuji Apples"
+                        src={fujiApples}
+                        quality={10}
+                        className="z-30 w-full rounded-full"
+                      />
+                      <div className="absolute px-1 font-bold rounded-full -top-1 -right-3 text-sm md:text-base bg-accent text-accent-foreground">
+                        {numeral(supplyPerEpoch / Math.pow(10, 6)).format('0a')}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div>
+                  <CardTitle className="z-30 my-2 text-xl font-semibold">Epoch #{currentEpoch}</CardTitle>
+
+                  <CardDescription className="z-30 mb-4 text-sm font-fine text-foreground space-y-2">
+                    <Progress value={epochProgress} />
+                    <p>Highest Bid: {highestBidder ? `${highestBidder.slice(-4)} at ${highestBid}` : 'None'}</p>
+                  </CardDescription>
+                </div>
+              </div>
+            </CardHeader>
+
+
+
+            <CardContent className="z-20 flex-grow p-4">
+              <div className="flex flex-col items-center justify-center space-y-4">
+
+                <p className='max-w-60'>
+                  {descriptionVisible && <Typewriter
+                    options={{ autoStart: true }}
+                    onInit={typewriter => {
+                      typewriter.pauseFor(2700).start().typeString('Brothers, what we do in life... echoes in eternity.')
+                    }}
+                  />}
+                </p>
+              </div>
+            </CardContent>
+
+            <CardFooter className="z-20 flex justify-between p-4 items-end">
+
+              <Link href="/crafting">
+                <Button variant="ghost" className="z-30">
+                  Back
+                </Button>
+              </Link>
+
+              <SelectCreatureDialog />
+            </CardFooter>
+            <Image
+              src={charismaticCorgi}
+              width={800}
+              height={1600}
+              alt={'quest-background-image'}
+              className={cn(
+                'object-cover',
+                'opacity-10',
+                'aspect-[2/3]',
+                'sm:aspect-square',
+                'flex',
+                'z-10',
+                'absolute',
+                'inset-0',
+                'pointer-events-none'
+              )}
+            />
+            <div className="absolute inset-0 z-0 pointer-events-none bg-gradient-to-b from-white to-black opacity-10" />
+          </Card>
+        </motion.div>
+      </Layout>
+    </Page>
+  );
+}
+
+type Props = {
+  data: any;
+};
+
+export const getStaticProps: GetStaticProps<Props> = () => {
+  try {
+
+    const data = {
+    };
+
+    return {
+      props: {
+        data: data
+      },
+      revalidate: 60000
+    };
+  } catch (error) {
+    return {
+      props: {
+        data: {}
+      }
+    };
+  }
+};
+
+export function SelectCreatureDialog({ data }: any) {
+
+  const { doContractCall } = useConnect();
+
+  const sender = userSession.isUserSignedIn() && userSession.loadUserData().profile.stxAddress.mainnet
+
+  const [farmerClaimableAmount, setFarmerClaimableAmount] = useState(0)
+  const [blacksmithClaimableAmount, setBlacksmithClaimableAmount] = useState(0)
+  const [corgiSoldierClaimableAmount, setCorgiSoldierClaimableAmount] = useState(0)
+  const [alchemistClaimableAmount, setAlchemistClaimableAmount] = useState(0)
+
+  useEffect(() => {
+
+    callReadOnlyFunction({
+      network: new StacksMainnet(),
+      contractAddress: "SP2ZNGJ85ENDY6QRHQ5P2D4FXKGZWCKTB2T0Z55KS",
+      contractName: 'battle-royale-v2',
+      functionName: "get-claimable-amount",
+      functionArgs: [uintCV(1)],
+      senderAddress: sender
+    }).then(response => setFarmerClaimableAmount(Number(cvToJSON(response).value)))
+
+    callReadOnlyFunction({
+      network: new StacksMainnet(),
+      contractAddress: "SP2ZNGJ85ENDY6QRHQ5P2D4FXKGZWCKTB2T0Z55KS",
+      contractName: 'battle-royale-v2',
+      functionName: "get-claimable-amount",
+      functionArgs: [uintCV(2)],
+      senderAddress: sender
+    }).then(response => setBlacksmithClaimableAmount(Number(cvToJSON(response).value)))
+
+    callReadOnlyFunction({
+      network: new StacksMainnet(),
+      contractAddress: "SP2ZNGJ85ENDY6QRHQ5P2D4FXKGZWCKTB2T0Z55KS",
+      contractName: 'battle-royale-v2',
+      functionName: "get-claimable-amount",
+      functionArgs: [uintCV(3)],
+      senderAddress: sender
+    }).then(response => setCorgiSoldierClaimableAmount(Number(cvToJSON(response).value)))
+
+    callReadOnlyFunction({
+      network: new StacksMainnet(),
+      contractAddress: "SP2ZNGJ85ENDY6QRHQ5P2D4FXKGZWCKTB2T0Z55KS",
+      contractName: 'battle-royale-v2',
+      functionName: "get-claimable-amount",
+      functionArgs: [uintCV(4)],
+      senderAddress: sender
+    }).then(response => setAlchemistClaimableAmount(Number(cvToJSON(response).value)))
+
+  }, [sender])
+
+  function journey(creatureId: number) {
+    // const response = await callReadOnlyFunction({
+    //   network: new StacksMainnet(),
+    //   contractAddress: "SP2ZNGJ85ENDY6QRHQ5P2D4FXKGZWCKTB2T0Z55KS",
+    //   contractName: 'battle-royale-v2',
+    //   functionName: "get-claimable-amount",
+    //   functionArgs: [uintCV(creatureId)],
+    //   senderAddress: sender
+    // })
+    // const claimableTokens = Number(cvToJSON(response).value)
+    doContractCall({
+      network: new StacksMainnet(),
+      anchorMode: AnchorMode.Any,
+      contractAddress: "SP2ZNGJ85ENDY6QRHQ5P2D4FXKGZWCKTB2T0Z55KS",
+      contractName: 'battle-royale-v2',
+      functionName: "battle",
+      functionArgs: [uintCV(creatureId)],
+      postConditionMode: PostConditionMode.Allow,
+      postConditions: [],
+      onFinish: (data) => {
+        console.log("onFinish:", data);
+      },
+      onCancel: () => {
+        console.log("onCancel:", "Transaction was canceled");
+      },
+    });
+  }
+
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button size={'sm'} className={`z-30`}>Battle for riches and glory</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-2xl">
+        <AlertDialogHeader>
+          <DialogTitle>Which creatures should you use?</DialogTitle>
+        </AlertDialogHeader>
+
+        <DialogDescription className='grid gap-2 grid-cols-2 sm:grid-cols-4 space-x-4 py-4'>
+          <div className='flex flex-col items-center space-y-2'>
+            <Image
+              alt={'asd'}
+              src={farmersImg}
+              width={100}
+              height={100}
+              onClick={() => journey(1)}
+              className="z-30 border rounded-lg h-32 w-32 hover:scale-110 transition-all cursor-pointer"
+            />
+            <h2 className='text-base text-primary-foreground'>Farmers</h2>
+            <div className='flex items-center gap-2 text-lg text-muted/80 font-semibold mr-4'>
+              <Image
+                alt={'Experience Icon'}
+                src={energyIcon}
+                width={100}
+                height={100}
+                className={`z-30 border rounded-full h-6 w-6`}
+              />
+              <div>{numeral(farmerClaimableAmount).format('0a')}</div>
+            </div>
+          </div>
+          <div className='flex flex-col items-center space-y-2'>
+            <Image
+              alt={'asd'}
+              src={blacksmithsImg}
+              width={100}
+              height={100}
+              onClick={() => journey(2)}
+              className="z-30 border rounded-lg h-32 w-32 hover:scale-110 transition-all cursor-pointer"
+            />
+            <h2 className='text-base text-primary-foreground'>Blacksmiths</h2>
+            <div className='flex items-center gap-2 text-lg text-muted/80 font-semibold mr-4'>
+              <Image
+                alt={'Experience Icon'}
+                src={energyIcon}
+                width={100}
+                height={100}
+                className={`z-30 border rounded-full h-6 w-6`}
+              />
+              <div>{numeral(blacksmithClaimableAmount).format('0a')}</div>
+            </div>
+          </div>
+          <div className='flex flex-col items-center space-y-2'>
+            <Image
+              alt={'asd'}
+              src={corgiSoldiersImg}
+              width={100}
+              height={100}
+              onClick={() => journey(3)}
+              className="z-30 border rounded-lg h-32 w-32 hover:scale-110 transition-all cursor-pointer"
+            />
+            <h2 className='text-base text-primary-foreground'>Corgi Soldiers</h2>
+            <div className='flex items-center gap-2 text-lg text-muted/80 font-semibold mr-4'>
+              <Image
+                alt={'Experience Icon'}
+                src={energyIcon}
+                width={100}
+                height={100}
+                className={`z-30 border rounded-full h-6 w-6`}
+              />
+              <div>{numeral(corgiSoldierClaimableAmount).format('0a')}</div>
+            </div>
+          </div>
+          <div className='flex flex-col items-center space-y-2'>
+            <Image
+              alt={'asd'}
+              src={alchemistsImg}
+              width={100}
+              height={100}
+              onClick={() => journey(4)}
+              className="z-30 border rounded-lg h-32 w-32 hover:scale-110 transition-all cursor-pointer"
+            />
+            <h2 className='text-base text-primary-foreground'>Alchemists</h2>
+            <div className='flex items-center gap-2 text-lg text-muted/80 font-semibold mr-4'>
+              <Image
+                alt={'Experience Icon'}
+                src={energyIcon}
+                width={100}
+                height={100}
+                className={`z-30 border rounded-full h-6 w-6`}
+              />
+              <div>{numeral(alchemistClaimableAmount).format('0a')}</div>
+            </div>
+          </div>
+        </DialogDescription>
+      </DialogContent>
+    </Dialog >
+  )
+}
