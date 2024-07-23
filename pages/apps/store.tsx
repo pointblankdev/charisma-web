2106f65366ff27883ac5769d213b89c829dd2e5f
Ross Ragsdale <ross@pointblankdev.com>
2024-07-23 17:59:31 -0500

    :art: Update

---
+++ pages/apps/store.tsx
@@ -0,0 +1,82 @@
+
+import { SkipNavContent } from '@reach/skip-nav';
+
+import Page from '@components/page';
+import { META_DESCRIPTION } from '@lib/constants';
+import Layout from '@components/layout';
+import { Card } from '@components/ui/card';
+import { useEffect, useState } from 'react';
+import { StacksMainnet } from "@stacks/network";
+import {
+  AnchorMode,
+  Pc,
+  PostConditionMode,
+  noneCV,
+  principalCV,
+  uintCV,
+} from "@stacks/transactions";
+import ConnectWallet, { userSession } from "@components/stacks-session/connect";
+import { Button } from "@components/ui/button";
+import { useConnect } from '@stacks/connect-react';
+
+
+export default function App() {
+  const meta = {
+    title: 'Charisma | DMG Transfer',
+    description: META_DESCRIPTION,
+  };
+
+  return (
+    <Page meta={meta} fullViewport>
+      <SkipNavContent />
+      <Layout>
+        <div className="m-2 sm:container sm:mx-auto sm:py-10 md:max-w-2xl">
+
+          <Card className='p-0 overflow-hidden bg-black text-primary-foreground border-accent-foreground rounded-xl'>
+            <div className='m-2'>
+              <div className='space-y-1'>
+                <Transfer />
+              </div>
+            </div>
+          </Card>
+        </div>
+      </Layout>
+    </Page>
+  );
+}
+
+// 1 sCHA = 1.071875 CHA
+
+const Transfer = () => {
+  const { doContractCall } = useConnect();
+
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => { setMounted(true) }, []);
+
+  function deposit() {
+    doContractCall({
+      network: new StacksMainnet(),
+      anchorMode: AnchorMode.Any,
+      contractAddress: "SP2ZNGJ85ENDY6QRHQ5P2D4FXKGZWCKTB2T0Z55KS",
+      contractName: 'creatures-kit',
+      functionName: "store",
+      functionArgs: [principalCV('SP1Y5YSTAHZ88XYK1VPDH24GY0HPX5J4JECTMY4A1.wstx-wcha')],
+      postConditionMode: PostConditionMode.Allow,
+      postConditions: [],
+      onFinish: (data) => {
+        console.log("onFinish:", data);
+      },
+      onCancel: () => {
+        console.log("onCancel:", "Transaction was canceled");
+      },
+    });
+  }
+
+  if (!mounted || !userSession.isUserSignedIn()) {
+    return <ConnectWallet />;
+  }
+
+  return (
+    <Button className='text-md w-full hover:bg-[#ffffffee] hover:text-primary' onClick={deposit}>Transfer</Button>
+  );
+};
