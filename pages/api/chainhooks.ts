db159b9dd0741677085d856163179db13ca6598f
Ross Ragsdale <ross@pointblankdev.com>
2024-07-24 20:23:53 -0500

    :sparkles: Add discord webhook for harvests

--- pages/api/chainhooks.ts
+++ pages/api/chainhooks.ts
@@ -1,4 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { Webhook, MessageBuilder } from 'discord-webhook-node'
+
+const hook = new Webhook('https://discord.com/api/webhooks/1144890336594907146/BtXYwXDuHsWt6IFMOylwowcmCUWjOoIM6MiqdIBqIdrbT5w_ui3xdxSP2OSc2DhlkDhn');
 
 type ErrorResponse = {
   error: {
@@ -32,6 +35,21 @@ export default function chainhooks(
         };
 
         console.log(payload)
+        // {
+        //   args: [ 'u1' ],
+        //   contract_identifier: 'SP2ZNGJ85ENDY6QRHQ5P2D4FXKGZWCKTB2T0Z55KS.abundant-orchard',
+        //   method: 'harvest',
+        //   sender: 'SP3T1M18J3VX038KSYPP5G450WVWWG9F9G6GAZA4Q',
+        //   success: true
+        // }
+
+        const embed = new MessageBuilder()
+          .setTitle('Harvest')
+          .setAuthor(payload.sender)
+          .setDescription('Creatures have havested FUJI tokens')
+          .setTimestamp();
+
+        payload.success === true && hook.send(embed);
       }
     }
   } catch (error: any) {
