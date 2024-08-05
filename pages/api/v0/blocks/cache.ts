aa76e02fe907b0f1d5ef32f03fdcf8d75e0df5ca
Ross Ragsdale <ross@pointblankdev.com>
2024-08-05 06:44:42 -0500

    :sparkles: Add block cacher.

---
+++ pages/api/v0/blocks/cache.ts
@@ -0,0 +1,31 @@
+import { blocksApi } from '@lib/stacks-api';
+import { setLatestBlock } from '@lib/user-api';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+type ErrorResponse = {
+    error: {
+        code: string;
+        message: string;
+    };
+};
+
+export default async function cacheLatestBlock(
+    req: NextApiRequest,
+    res: NextApiResponse<any | ErrorResponse>
+) {
+
+    const response: any = {}
+    try {
+
+        const { results } = await blocksApi.getBlocks({ limit: 1 })
+
+        await setLatestBlock(results[0])
+
+        response.block = results[0]
+
+    } catch (error: any) {
+        console.error(error.message);
+    }
+
+    return res.status(200).json(response);
+}
