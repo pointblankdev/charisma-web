6fe32c8631b62a5d9df3b85444673345b60bcf2f
Ross Ragsdale <ross@pointblankdev.com>
2024-08-14 05:22:39 -0500

    :art: Update

--- pages/api/v0/mobs/index.ts
+++ pages/api/v0/mobs/index.ts
@@ -119,7 +119,7 @@ export default async function getMetadata(
                             if (event.type === 'SmartContractEvent' && 'topic' in event.data) {
                                 // loop through all values in the value object
                                 if (typeof event.data.value === 'object' && event.data.value !== null) {
-                                    embed.addField(':charisma: Event', '—');
+                                    embed.addField('🚩 Charisma Event', '');
                                     Object.entries(event.data.value).forEach(([key, value]) => {
                                         // Convert the value to a string, handling potential nested objects
                                         const stringValue = typeof value === 'object' ? JSON.stringify(value) : String(value);
