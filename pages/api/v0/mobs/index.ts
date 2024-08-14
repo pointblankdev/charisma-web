a1b66fde3171395d67bd854e9090017ed8d167d6
Ross Ragsdale <ross@pointblankdev.com>
2024-08-14 07:47:10 -0500

    :art: Update

--- pages/api/v0/mobs/index.ts
+++ pages/api/v0/mobs/index.ts
@@ -83,30 +83,30 @@ export default async function getMetadata(
                         .setDescription(tx.metadata.description)
                         .setThumbnail('https://beta.charisma.rocks/quests/wanted-hogger/hogger.png')
 
-                    // for (const event of tx.metadata.receipt.events) {
-                    //     if (event.type === 'SmartContractEvent') {
-                    //         if (event.data.value.event === 'attack-result') {
-                    //             const newHealth = event.data.value['new-hogger-health']
-                    //             const hogger = await getMob('hogger')
-                    //             if (hogger.maxHealth || 0 < newHealth) { hogger.maxHealth = newHealth }
-                    //             hogger.health = newHealth
-                    //             await setMob('hogger', hogger)
-                    //         }
-                    //         try {
-                    //             embed.addField(`🔻 ${event.data.value.event ? event.data.value.event : 'event'}`, '');
-                    //             Object.entries(event.data.value).forEach(([key, value]: any) => {
-                    //                 const stringValue = typeof value === 'object' ? safeJsonStringify(value) : String(value);
-                    //                 embed.addField(key, stringValue, true);
-                    //             });
-                    //         } catch (error) {
-                    //             embed.addField(event.data.topic, safeJsonStringify(event.data.value));
-                    //         }
-                    //     } else if (event.type === 'FTBurnEvent') {
-                    //         embed.addField('🔥 protocol-burn', `Burned ${event.data.amount / Math.pow(10, 6)} ${event.data.asset_identifier.split('.')[1].split('::')[0]} tokens.`);
-                    //     } else {
-                    //         embed.addField(event.type, safeJsonStringify(event.data));
-                    //     }
-                    // }
+                    for (const event of tx.metadata.receipt.events) {
+                        if (event.type === 'SmartContractEvent') {
+                            // if (event.data.value.event === 'attack-result') {
+                            //     const newHealth = event.data.value['new-hogger-health']
+                            //     const hogger = await getMob('hogger')
+                            //     if (hogger.maxHealth || 0 < newHealth) { hogger.maxHealth = newHealth }
+                            //     hogger.health = newHealth
+                            //     await setMob('hogger', hogger)
+                            // }
+                            // try {
+                            //     embed.addField(`🔻 ${event.data.value.event ? event.data.value.event : 'event'}`, '');
+                            //     Object.entries(event.data.value).forEach(([key, value]: any) => {
+                            //         const stringValue = typeof value === 'object' ? safeJsonStringify(value) : String(value);
+                            //         embed.addField(key, stringValue, true);
+                            //     });
+                            // } catch (error) {
+                            //     embed.addField(event.data.topic, safeJsonStringify(event.data.value));
+                            // }
+                        } else if (event.type === 'FTBurnEvent') {
+                            embed.addField('🔥 protocol-burn', `Burned ${event.data.amount / Math.pow(10, 6)} ${event.data.asset_identifier.split('.')[1].split('::')[0]} tokens.`);
+                        } else {
+                            embed.addField(event.type, safeJsonStringify(event.data));
+                        }
+                    }
 
                     await hook.send(embed);
                     response = {}
