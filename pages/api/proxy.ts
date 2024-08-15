eb4001f431f2c5180ca807d906da05ef1fa1a5dc
Ross Ragsdale <ross@pointblankdev.com>
2024-08-14 21:15:08 -0500

    :coffin: Remove dead API code

--- pages/api/proxy.ts
+++
@@ -1,9 +0,0 @@
-export default async function handler(req: any, res: any) {
-    try {
-        const response = await fetch(req.query.url);
-        const data = await response.json();
-        res.status(200).json(data);
-    } catch (error) {
-        res.status(500).json({ error: 'Failed to fetch data' });
-    }
-}
