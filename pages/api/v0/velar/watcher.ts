5ebe2be8748abb9a1f73e0e4d12c4496c013d8e2
Ross Ragsdale <ross@pointblankdev.com>
2024-08-19 06:15:11 -0500

    :art: Update

--- pages/api/v0/velar/watcher.ts
+++ pages/api/v0/velar/watcher.ts
@@ -87,6 +87,7 @@ export default async function velarWatcher(
                         await handleContractEvent(event, embed)
                     }
 
+
                     await hook.send(embed);
                     response = {}
 
