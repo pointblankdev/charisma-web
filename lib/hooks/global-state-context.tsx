abfb41e4b61d449203493078fc3497f037f791b7
Ross Ragsdale <ross@pointblankdev.com>
2024-08-23 14:45:33 -0500

    :art: Update

--- lib/hooks/global-state-context.tsx
+++ lib/hooks/global-state-context.tsx
@@ -39,7 +39,7 @@ export const GlobalStateProvider: React.FC<{ children: React.ReactNode }> = ({ c
         [key: string]: LandData;
     }
 
-    const stakedTokens = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    const stakedTokens = [1, 2, 3, 4, 5, 6, 7]
 
 
     useEffect(() => {
