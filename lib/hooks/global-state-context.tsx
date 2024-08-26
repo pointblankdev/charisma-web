c4c600a8156c59a25b7e3d6924375875aa6a7c3a
Ross Ragsdale <ross@pointblankdev.com>
2024-08-26 07:29:42 -0500

    :art: Include additional pools in header energy lookup

--- lib/hooks/global-state-context.tsx
+++ lib/hooks/global-state-context.tsx
@@ -39,7 +39,7 @@ export const GlobalStateProvider: React.FC<{ children: React.ReactNode }> = ({ c
         [key: string]: LandData;
     }
 
-    const stakedTokens = [1, 4, 5, 6, 7]
+    const stakedTokens = [1, 4, 5, 6, 7, 8, 9, 10, 11]
 
 
     useEffect(() => {
