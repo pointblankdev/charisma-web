7daa7916ac34a9ff7fe6321ea6c837b206a9cf05
firerum <ademuyiwaadewuyi@gmail.com>
2024-09-15 15:44:19 +0100

    conditional check

--- components/quest/token-select-dialog.tsx
+++ components/quest/token-select-dialog.tsx
@@ -30,7 +30,7 @@ export const TokenSelectDialog = ({ lands, contractId, buttonText = 'Complete Qu
         if (lands.length > 0) {
             for (const land of lands) {
                 land.balances = landEnergy[land.id];
-                console.log(`Land ID: ${land.id}, Energy: ${land.balances.energy}, Whitelisted: ${land.whitelisted}, Tapped: ${tapped[land.id]}`);
+                console.log(`Land ID: ${land.id}, Energy: ${land.balances?.energy}, Whitelisted: ${land.whitelisted}, Tapped: ${tapped[land.id]}`);
             }
         }
     }, [lands, landEnergy, tapped]);
