2a9c4dda9c1629c859b52ea3a218374615538d95
firerum <ademuyiwaadewuyi@gmail.com>
2024-09-16 11:44:05 +0100

    remove the conditional check

--- components/quest/token-select-dialog.tsx
+++ components/quest/token-select-dialog.tsx
@@ -116,11 +116,11 @@ export const TokenSelectDialog = ({ lands, contractId, buttonText = 'Complete Qu
                                 onClick={() => (tap(land.id))}
                                 className={`w-24 h-24 z-20 border rounded-full ${(land.whitelisted && !tapped[land.id] && land.balances?.energy) && 'group-hover/token:z-40 group-hover/token:shadow-xl group-hover/token:scale-110 transition-all'}`}
                             />
-                            {land.balances?.energy > 0 && (
-                                <div className={`z-30 opacity-0 absolute text-center px-3 py-1 border min-w-6 font-bold rounded-full top-0 text-md bg-card text-accent-foreground flex ${(land.whitelisted && !tapped[land.id] && land.balances?.energy) && 'group-hover/token:-top-6 group-hover/token:opacity-100 group-hover/token:z-50 group-hover/token:shadow-xl group-hover/token:scale-150 transition-all'}`}>
-                                    <div className='z-30 text-white whitespace-nowrap'>{numeral(land.balances?.energy).format('0a')} ⚡</div>
-                                </div>
-                            )}
+        
+                            <div className={`z-30 opacity-0 absolute text-center px-3 py-1 border min-w-6 font-bold rounded-full top-0 text-md bg-card text-accent-foreground flex ${(land.whitelisted && !tapped[land.id] && land.balances?.energy) && 'group-hover/token:-top-6 group-hover/token:opacity-100 group-hover/token:z-50 group-hover/token:shadow-xl group-hover/token:scale-150 transition-all'}`}>
+                                <div className='z-30 text-white whitespace-nowrap'>{numeral(land.balances?.energy).format('0a')} ⚡</div>
+                            </div>
+                           
                         </div>
                     ))}
                 </DialogDescription>
