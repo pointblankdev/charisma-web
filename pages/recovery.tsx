256f31931871813cbea69b7a89b6689d3bac8e02
Ross Ragsdale <rossragsdale@gmail.com>
2024-10-02 08:13:05 -0500

    :art: Improve redemptions page

--- pages/recovery.tsx
+++ pages/recovery.tsx
@@ -66,8 +66,6 @@ export default function RecoveryClaimPage({ data }: Props) {
     description: "Charisma Recovery",
   };
 
-  console.log(data)
-
   return (
     <Page meta={meta} fullViewport>
       <SkipNavContent />
@@ -107,6 +105,7 @@ const TokenRedemptions = ({ data }: any) => {
 
   const { openContractCall } = useOpenContractCall();
   const { stxAddress } = useAccount();
+  const { wallet } = useWallet();
 
   const [claimA, setClaimA] = useState(0);
   const [claimB, setClaimB] = useState(0);
@@ -227,35 +226,39 @@ const TokenRedemptions = ({ data }: any) => {
 
   return (
     <div className='max-w-5xl space-y-4'>
-      <div className="p-6 mx-auto space-y-8 rounded-lg shadow-lg bg-gray-900/80">
-        <div className='grid grid-cols-3'>
-          <div></div>
+      <div className="flex w-full p-6 rounded-lg shadow-lg bg-gray-900/80">
+        <div className='grid grid-cols-3 text-right grow'>
+          <div className='text-left'></div>
           <div>Synthetic Welsh (iouWELSH)</div>
           <div>Synthetic Roo (iouROO)</div>
 
-          <div>Total Issued</div>
+          <div className='text-left'>Total Issued</div>
           <div>{data.syntheticWelsh.issued / Math.pow(10, 6)}</div>
           <div>{data.syntheticRoo.issued / Math.pow(10, 6)}</div>
 
-          <div>Total Redeemed</div>
+          <div className='text-left'>Total Redeemed</div>
           <div>{data.syntheticWelsh.burned / Math.pow(10, 6)}</div>
           <div>{data.syntheticRoo.burned / Math.pow(10, 6)}</div>
 
-          <div>Redemptions Remaining</div>
+          <div className='text-left'>Redemptions Remaining</div>
           <div>{data.syntheticWelsh.remaining / Math.pow(10, 6)}</div>
           <div>{data.syntheticRoo.remaining / Math.pow(10, 6)}</div>
 
-          <div className='mt-4'>Your Balances</div>
+          <div className='mt-4 text-left'>Your Balances</div>
           <div className='mt-4'>{iouWelsh?.balance / Math.pow(10, 6) || 0}</div>
           <div className='mt-4'>{iouRoo?.balance / Math.pow(10, 6) || 0}</div>
 
-          <div>Redemptions Available</div>
+          <div className='text-left'>Redemptions Available</div>
           <div>{data.syntheticWelsh.available / Math.pow(10, 6)}</div>
           <div>{data.syntheticRoo.available / Math.pow(10, 6)}</div>
 
-          <div className='mt-4'>Redeem Tokens</div>
-          <div className='mt-4'><Button disabled={data.syntheticWelsh.available === 0} onClick={() => redeemWelsh(10000000000)} className='h-6' >Redeem iouWELSH</Button></div>
-          <div className='mt-4'><Button disabled={data.syntheticRoo.available === 0} onClick={() => redeemRoo(100000000)} className='h-6' >Redeem iouROO</Button></div>
+          <div className='mt-4 text-left'>Redeem Tokens</div>
+          <div className='mt-4'><Button disabled={data.syntheticWelsh.available === 0 || !wallet.bluePilled} onClick={() => redeemWelsh(10000000000)} className='h-6 bg-blue-800' >Redeem iouWELSH</Button></div>
+          <div className='mt-4'><Button disabled={data.syntheticRoo.available === 0 || !wallet.bluePilled} onClick={() => redeemRoo(100000000)} className='h-6 bg-blue-800' >Redeem iouROO</Button></div>
+        </div>
+        <div className='ml-6'>
+          <div className='text-center'>Blue Pilled</div>
+          {wallet.bluePilled ? <Image src={bluePillFloating} alt='Blue Pill' width={120} height={120} /> : <div className='m-2 text-center'>❌</div>}
         </div>
       </div>
 
