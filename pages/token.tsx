d7928ac1785ca7cd2be162ef3ab81ae6b5bc7f2a
Ross Ragsdale <ross@pointblankdev.com>
2024-10-06 11:13:26 -0500

    :art: Update

--- pages/token.tsx
+++ pages/token.tsx
@@ -148,7 +148,7 @@ const StatsSection = () => {
   return (
     <div>
       <div className='w-full pt-4 text-3xl font-bold text-center uppercase'>WIN THE BLOCK</div>
-      <div className='w-full pb-8 text-center text-md text-muted/90'>Bid for a chance to win the block reward.</div>
+      <div className='w-full pb-8 text-center text-md text-muted/90'>Bid in the mempool for a chance to win the block.</div>
       <div className='grid grid-cols-2 gap-4 sm:grid-cols-4'>
         <div className='flex flex-col items-center justify-center p-4 space-y-2 rounded-lg text-md bg-[var(--sidebar)] border border-[var(--accents-7)]'>
           <div className='flex items-center space-x-0'>
