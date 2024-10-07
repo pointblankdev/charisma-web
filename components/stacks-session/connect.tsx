5a3904fe66ed0e85bb081eea7302013e60994694
Ross Ragsdale <rossragsdale@gmail.com>
2024-10-07 06:39:34 -0500

    :art: Update

--- components/stacks-session/connect.tsx
+++ components/stacks-session/connect.tsx
@@ -8,16 +8,22 @@ import {
 } from "@components/ui/navigation-menu"
 import { useAccount, useAuth } from '@micro-stacks/react';
 import ListItem from "./list-item";
+import { useRouter } from "next/navigation";
 
 const ConnectWallet = () => {
   const { openAuthRequest, isRequestPending, signOut, isSignedIn } = useAuth();
   const { stxAddress } = useAccount();
+  const router = useRouter();
 
   const shortAddress = `${stxAddress?.slice(0, 4)}...${stxAddress?.slice(-4)}`
 
   const handleConnect = async () => {
-    if (isSignedIn) await signOut();
-    else await openAuthRequest();
+    if (isSignedIn) await signOut(() => { router.push('/') })
+    else await openAuthRequest({
+      onFinish: () => {
+        router.push('/token')
+      }
+    });
   }
   const label = isRequestPending ? 'Loading...' : isSignedIn ? shortAddress : 'Connect';
 
@@ -44,7 +50,7 @@ const ConnectWallet = () => {
               <ListItem href="/governance" title="Governance">
                 Vote on DAO proposals using the Charisma token.
               </ListItem>
-              <ListItem title="Sign Out" onClick={() => signOut()} className="cursor-pointer">
+              <ListItem title="Sign Out" onClick={() => signOut(() => { router.push('/') })} className="cursor-pointer">
                 Securely disconnect your wallet.
               </ListItem>
             </ul>
