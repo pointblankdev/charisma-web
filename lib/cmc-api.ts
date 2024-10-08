cbbda42ba9f8c78496627f88f89b6bdc94edcb99
Ross Ragsdale <ross@pointblankdev.com>
2024-10-08 17:11:07 -0500

    :art: Update

--- lib/cmc-api.ts
+++ lib/cmc-api.ts
@@ -2,7 +2,7 @@
 //@ts-ignore
 import CoinMarketCap from 'coinmarketcap-api'
 
-const apiKey = '97a589da-791f-43bd-8b81-6d01cd304090'
+const apiKey = process.env.CMC_API_KEY
 const cmc = new CoinMarketCap(apiKey)
 
 export default cmc
