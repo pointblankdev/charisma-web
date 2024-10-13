7cb3d7a389d6546c1cd3a2fcd929ec1852aff5e2
Ross Ragsdale <rossragsdale@gmail.com>
2024-10-13 05:38:06 -0500

    :fire: Remove wrap tab heder

--- lib/constants.ts
+++ lib/constants.ts
@@ -21,10 +21,10 @@ const VERCEL_URL = process.env.NEXT_PUBLIC_VERCEL_URL;
 export const API_URL = VERCEL_URL ? `https://${VERCEL_URL}` : 'http://localhost:3000';
 
 export const NAVIGATION = [
-  {
-    name: 'Wrap',
-    route: '/token'
-  },
+  // {
+  //   name: 'Wrap',
+  //   route: '/token'
+  // },
   {
     name: 'Swap',
     route: '/swap'
