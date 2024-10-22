6837dc3bbcd694c56a8b0f7fe718bc930d7378d0
Ross Ragsdale <ross@pointblankdev.com>
2024-10-22 04:22:41 -0500

    :art: Update

---
+++ pages/api/v0/interactions/charismatic-corgi.ts
@@ -0,0 +1,23 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+type ErrorResponse = {
+    error: {
+        code: string;
+        message: string;
+    };
+};
+
+export default function InteractionAPI(
+    req: NextApiRequest,
+    res: NextApiResponse<any | ErrorResponse>
+) {
+    return res.status(200).json({
+        url: "https://charisma.rocks/interactions/charismatic-corgi",
+        image: "https://charisma.rocks/interactions/charismatic-corgi.png",
+        name: "Charismatic Corgi",
+        description: "Automated arbitrage yielding WELSH and CHA tokens.",
+        contract: "SP2ZNGJ85ENDY6QRHQ5P2D4FXKGZWCKTB2T0Z55KS.charismatic-corgi-rc1",
+        category: "Rewards",
+        actions: ["FORWARD", "REVERSE"]
+    });
+}
