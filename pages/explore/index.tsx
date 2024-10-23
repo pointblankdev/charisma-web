9709eb88ad90e0dbc6070b9961e5e2dfaf756877
Ross Ragsdale <ross@pointblankdev.com>
2024-10-23 02:04:44 -0500

    :art: Update

--- pages/explore/index.tsx
+++ pages/explore/index.tsx
@@ -725,7 +725,7 @@ function Sidebar({ className, collections }: SidebarProps) {
             )}
 
             {/* Characters list */}
-            <ScrollArea className="h-[200px]">
+            <ScrollArea className="h-fit">
               {isLoading ? (
                 <div className="flex items-center justify-center h-full">
                   <svg className="animate-spin h-5 w-5 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
