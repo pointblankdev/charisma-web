349adf7b706837e39a29cea7c42978cb115f093d
Ross Ragsdale <rossragsdale@gmail.com>
2024-11-08 15:43:56 -0600

    :art: Update

--- pages/api/v0/prices/index.ts
+++ pages/api/v0/prices/index.ts
@@ -86,6 +86,8 @@ export default async function handler(
       dexClient.getPoolById('4')
     ]);
 
+    console.log('Fetched data:', stxChaPool);
+
     // Calculate CHA price from pool ratio
     const ratio = Number(stxChaPool.reserve0) / Number(stxChaPool.reserve1);
 
