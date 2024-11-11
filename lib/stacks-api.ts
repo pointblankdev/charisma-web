e2c90796598a66e0e63f6a775836c253e6ace210
Ross Ragsdale <rossragsdale@gmail.com>
2024-11-11 14:47:05 -0600

    :art: Update

--- lib/stacks-api.ts
+++ lib/stacks-api.ts
@@ -113,8 +113,10 @@ export async function getNftURI(
     });
     const cv = cvToJSON(hexToCV(response.data.result));
     const url = cv.value.value.value.replace('{id}', tokenId);
-    const result = await fetch(url, { mode: 'no-cors', redirect: 'follow' });
-    return await result.json();
+    const result = await fetch(`https://corsproxy.io/?${url}`, { mode: 'no-cors' });
+    const out = await result.json();
+    console.log(out);
+    return out;
   } catch (error) {
     console.error('Fetch error:', error);
     throw error;
