b673badffbb08ab007c619c740f7aa3f4756dade
Ross Ragsdale <rossragsdale@gmail.com>
2024-11-10 18:00:29 -0600

    :art: Update

--- components/pools/layout.tsx
+++ components/pools/layout.tsx
@@ -132,7 +132,7 @@ export default function PoolsLayout({ children }: any) {
                   icon={LayersIcon}
                   title="Derivative Pools"
                   subtitle="Multi-Asset Exposure"
-                  description="These pools feature liquidity provider (LP) tokens as swappable assets. These pools will generally have higher APYs and less volatile price movement."
+                  description="These pools feature liquidity provider (LP) tokens as swappable assets. These pools will offer more consistent APYs and lower average price volatility."
                   protocolFee={0.25}
                   lpYield={0.25}
                 />
