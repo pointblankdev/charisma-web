9dde57702aaedc8f9725475a7908d5aad0f7333c
Ross Ragsdale <rossragsdale@gmail.com>
2024-11-13 16:41:33 -0600

    :iphone: Improve swap responsiveness

--- components/swap/swap-interface.tsx
+++ components/swap/swap-interface.tsx
@@ -51,7 +51,7 @@ interface TokenListProps {
 }
 
 const TokenList = ({ tokens, onSelect, fromToken, hasHighExperience, pools }: TokenListProps) => (
-  <div className="absolute right-0 z-10 w-full mt-2 overflow-hidden rounded-md shadow-lg bg-[var(--sidebar)] border border-primary/30 min-w-[40rem] grid grid-cols-4">
+  <div className="absolute right-0 z-10 w-full mt-2 overflow-hidden rounded-md shadow-lg bg-[var(--sidebar)] border border-primary/30 min-w-[22rem] sm:min-w-[40rem] grid grid-cols-2 sm:grid-cols-4">
     {tokens.map(token => {
       const isDisabled =
         fromToken &&
