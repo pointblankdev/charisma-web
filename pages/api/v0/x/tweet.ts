15fd70e696e47a0f790096e0c953fe274c4ed8bf
Ross Ragsdale <rossragsdale@gmail.com>
2024-12-11 00:45:00 -0600

    :art: Update

--- pages/api/v0/x/tweet.ts
+++ pages/api/v0/x/tweet.ts
@@ -19,7 +19,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
 
   try {
     // Get chat response from Charisma API
-    const charismaResponse = await fetch('http://localhost:3001/api/chat', {
+    const charismaResponse = await fetch('https://explore.charisma.rocks/api/chat', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
