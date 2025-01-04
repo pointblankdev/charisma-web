6da7c5344da6cdf74d62b7c6aa5e635b3d253787
Ross Ragsdale <rossragsdale@gmail.com>
2025-01-04 05:56:13 -0600

    :construction: Draft up sub-operations logic with reward engines

--- lib/codegen/dexterity.ts
+++ lib/codegen/dexterity.ts
@@ -80,14 +80,21 @@ export function generateContractCode(params: ContractParams): string {
 (define-constant LP_REBATE u${lpRebateRaw})
 
 ;; Operation Types (Byte 0 of opcode)
-(define-constant OP_SWAP_A_TO_B 0x00)     ;; Swap token A for B
-(define-constant OP_SWAP_B_TO_A 0x01)     ;; Swap token B for A
-(define-constant OP_ADD_LIQUIDITY 0x02)   ;; Add liquidity
-(define-constant OP_REMOVE_LIQUIDITY 0x03) ;; Remove liquidity
+(define-constant OP_SWAP_A_TO_B 0x00)       ;; Swap token A for B
+(define-constant OP_SWAP_B_TO_A 0x01)       ;; Swap token B for A
+(define-constant OP_ADD_LIQUIDITY 0x02)     ;; Add liquidity
+(define-constant OP_REMOVE_LIQUIDITY 0x03)  ;; Remove liquidity
+
+;; Sub Operations (Byte 1 of opcode)
+(define-constant OP_TAP_TOKENS 0x00)        ;; Tap tokens for energy
 
 ;; Define LP token
 (define-fungible-token ${lpTokenSymbol})
 (define-data-var token-uri (optional (string-utf8 256)) (some u"${tokenUri}"))
+
+;; Reward Engine State
+(define-data-var first-start-block uint stacks-block-height)
+(define-map last-tap-block principal uint)
 
 ;; --- SIP10 Functions ---
 
@@ -126,7 +133,9 @@ export function generateContractCode(params: ContractParams): string {
 (define-public (execute (amount uint) (opcode (optional (buff 16))))
     (let (
         (sender tx-sender)
-        (operation (get-byte opcode u0)))
+        (operation (get-byte opcode u0))
+        (sub-operation (get-byte opcode u1)))
+        (if (is-eq sub-operation OP_TAP_TOKENS) (match (tap-tokens u0) success true error false) false)
         (if (is-eq operation OP_SWAP_A_TO_B) (swap-a-to-b amount)
         (if (is-eq operation OP_SWAP_B_TO_A) (swap-b-to-a amount)
         (if (is-eq operation OP_ADD_LIQUIDITY) (add-liquidity amount)
@@ -182,6 +191,25 @@ export function generateContractCode(params: ContractParams): string {
         ${generateTransferOut(isTokenBStx, tokenBContract, '(get dy delta)', 'CONTRACT', 'sender')}
         (ok delta)))
 
+(define-public (tap-tokens (block-offset uint))
+    (let (
+        (sender tx-sender)
+        (start-block (get-last-tap-block sender))
+        (end-block (- stacks-block-height block-offset))
+        (block-delta (- end-block start-block))
+        (balance-integral (calculate-balance-integral sender start-block end-block))
+        (potential-energy (/ balance-integral (ft-get-supply ${lpTokenSymbol}))))
+        (map-set last-tap-block sender (- stacks-block-height block-offset))
+        (print {
+          op: "TAP_TOKENS", 
+          sender: sender, 
+          energy: potential-energy, 
+          integral: balance-integral, 
+          block-delta: block-delta, 
+          message: "The tokens resonate with power, and produce energy for their holder."
+        })
+        (contract-call? 'SP2ZNGJ85ENDY6QRHQ5P2D4FXKGZWCKTB2T0Z55KS.charisma-rulebook-v0 energize potential-energy sender)))
+
 ;; --- Helper Functions ---
 
 (define-private (get-byte (opcode (optional (buff 16))) (position uint))
@@ -192,6 +220,147 @@ export function generateContractCode(params: ContractParams): string {
       a: ${getBalance(isTokenAStx, tokenAContract, 'CONTRACT')}, 
       b: ${getBalance(isTokenBStx, tokenBContract, 'CONTRACT')}
     })
+
+(define-private (get-balance (data { address: principal, block: uint }))
+    (let ((target-block (get block data)))
+        (if (< target-block stacks-block-height)
+            (let ((block-hash (unwrap-panic (get-stacks-block-info? id-header-hash target-block))))
+                (at-block block-hash (ft-get-balance ${lpTokenSymbol} (get address data))))
+                (ft-get-balance ${lpTokenSymbol} (get address data)))))
+
+(define-private (calculate-trapezoid-areas-39 (balances (list 39 uint)) (dx uint))
+    (list
+        (/ (* (+ (unwrap-panic (element-at balances u0)) (unwrap-panic (element-at balances u1))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u1)) (unwrap-panic (element-at balances u2))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u2)) (unwrap-panic (element-at balances u3))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u3)) (unwrap-panic (element-at balances u4))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u4)) (unwrap-panic (element-at balances u5))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u5)) (unwrap-panic (element-at balances u6))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u6)) (unwrap-panic (element-at balances u7))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u7)) (unwrap-panic (element-at balances u8))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u8)) (unwrap-panic (element-at balances u9))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u9)) (unwrap-panic (element-at balances u10))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u10)) (unwrap-panic (element-at balances u11))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u11)) (unwrap-panic (element-at balances u12))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u12)) (unwrap-panic (element-at balances u13))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u13)) (unwrap-panic (element-at balances u14))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u14)) (unwrap-panic (element-at balances u15))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u15)) (unwrap-panic (element-at balances u16))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u16)) (unwrap-panic (element-at balances u17))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u17)) (unwrap-panic (element-at balances u18))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u18)) (unwrap-panic (element-at balances u19))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u19)) (unwrap-panic (element-at balances u20))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u20)) (unwrap-panic (element-at balances u21))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u21)) (unwrap-panic (element-at balances u22))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u22)) (unwrap-panic (element-at balances u23))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u23)) (unwrap-panic (element-at balances u24))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u24)) (unwrap-panic (element-at balances u25))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u25)) (unwrap-panic (element-at balances u26))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u26)) (unwrap-panic (element-at balances u27))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u27)) (unwrap-panic (element-at balances u28))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u28)) (unwrap-panic (element-at balances u29))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u29)) (unwrap-panic (element-at balances u30))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u30)) (unwrap-panic (element-at balances u31))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u31)) (unwrap-panic (element-at balances u32))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u32)) (unwrap-panic (element-at balances u33))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u33)) (unwrap-panic (element-at balances u34))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u34)) (unwrap-panic (element-at balances u35))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u35)) (unwrap-panic (element-at balances u36))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u36)) (unwrap-panic (element-at balances u37))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u37)) (unwrap-panic (element-at balances u38))) dx) u2)))
+
+(define-private (calculate-trapezoid-areas-19 (balances (list 19 uint)) (dx uint))
+    (list
+        (/ (* (+ (unwrap-panic (element-at balances u0)) (unwrap-panic (element-at balances u1))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u1)) (unwrap-panic (element-at balances u2))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u2)) (unwrap-panic (element-at balances u3))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u3)) (unwrap-panic (element-at balances u4))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u4)) (unwrap-panic (element-at balances u5))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u5)) (unwrap-panic (element-at balances u6))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u6)) (unwrap-panic (element-at balances u7))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u7)) (unwrap-panic (element-at balances u8))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u8)) (unwrap-panic (element-at balances u9))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u9)) (unwrap-panic (element-at balances u10))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u10)) (unwrap-panic (element-at balances u11))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u11)) (unwrap-panic (element-at balances u12))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u12)) (unwrap-panic (element-at balances u13))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u13)) (unwrap-panic (element-at balances u14))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u14)) (unwrap-panic (element-at balances u15))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u15)) (unwrap-panic (element-at balances u16))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u16)) (unwrap-panic (element-at balances u17))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u17)) (unwrap-panic (element-at balances u18))) dx) u2)))
+
+(define-private (calculate-trapezoid-areas-9 (balances (list 9 uint)) (dx uint))
+    (list
+        (/ (* (+ (unwrap-panic (element-at balances u0)) (unwrap-panic (element-at balances u1))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u1)) (unwrap-panic (element-at balances u2))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u2)) (unwrap-panic (element-at balances u3))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u3)) (unwrap-panic (element-at balances u4))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u4)) (unwrap-panic (element-at balances u5))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u5)) (unwrap-panic (element-at balances u6))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u6)) (unwrap-panic (element-at balances u7))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u7)) (unwrap-panic (element-at balances u8))) dx) u2)))
+
+(define-private (calculate-trapezoid-areas-5 (balances (list 5 uint)) (dx uint))
+    (list
+        (/ (* (+ (unwrap-panic (element-at balances u0)) (unwrap-panic (element-at balances u1))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u1)) (unwrap-panic (element-at balances u2))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u2)) (unwrap-panic (element-at balances u3))) dx) u2)
+        (/ (* (+ (unwrap-panic (element-at balances u3)) (unwrap-panic (element-at balances u4))) dx) u2)))
+
+(define-private (calculate-trapezoid-areas-2 (balances (list 2 uint)) (dx uint))
+    (list
+        (/ (* (+ (unwrap-panic (element-at balances u0)) (unwrap-panic (element-at balances u1))) dx) u2)))
+
+(define-private (calculate-balance-integral-39 (address principal) (start-block uint) (end-block uint))
+    (let (
+        (sample-points (contract-call? 'SP2ZNGJ85ENDY6QRHQ5P2D4FXKGZWCKTB2T0Z55KS.meme-engine-manager generate-sample-points-39 address start-block end-block))
+        (balances (map get-balance sample-points))
+        (dx (/ (- end-block start-block) u38))
+        (areas (calculate-trapezoid-areas-39 balances dx)))
+        (fold + areas u0)))
+
+(define-private (calculate-balance-integral-19 (address principal) (start-block uint) (end-block uint))
+    (let (
+        (sample-points (contract-call? 'SP2ZNGJ85ENDY6QRHQ5P2D4FXKGZWCKTB2T0Z55KS.meme-engine-manager generate-sample-points-19 address start-block end-block))
+        (balances (map get-balance sample-points))
+        (dx (/ (- end-block start-block) u18))
+        (areas (calculate-trapezoid-areas-19 balances dx)))
+        (fold + areas u0)))
+
+(define-private (calculate-balance-integral-9 (address principal) (start-block uint) (end-block uint))
+    (let (
+        (sample-points (contract-call? 'SP2ZNGJ85ENDY6QRHQ5P2D4FXKGZWCKTB2T0Z55KS.meme-engine-manager generate-sample-points-9 address start-block end-block))
+        (balances (map get-balance sample-points))
+        (dx (/ (- end-block start-block) u8))
+        (areas (calculate-trapezoid-areas-9 balances dx)))
+        (fold + areas u0)))
+
+(define-private (calculate-balance-integral-5 (address principal) (start-block uint) (end-block uint))
+    (let (
+        (sample-points (contract-call? 'SP2ZNGJ85ENDY6QRHQ5P2D4FXKGZWCKTB2T0Z55KS.meme-engine-manager generate-sample-points-5 address start-block end-block))
+        (balances (map get-balance sample-points))
+        (dx (/ (- end-block start-block) u4))
+        (areas (calculate-trapezoid-areas-5 balances dx)))
+        (fold + areas u0)))
+
+(define-private (calculate-balance-integral-2 (address principal) (start-block uint) (end-block uint))
+    (let (
+        (sample-points (contract-call? 'SP2ZNGJ85ENDY6QRHQ5P2D4FXKGZWCKTB2T0Z55KS.meme-engine-manager generate-sample-points-2 address start-block end-block))
+        (balances (map get-balance sample-points))
+        (dx (/ (- end-block start-block) u1))
+        (areas (calculate-trapezoid-areas-2 balances dx)))
+        (fold + areas u0)))
+
+(define-private (calculate-balance-integral (address principal) (start-block uint) (end-block uint))
+    (let (
+        (block-delta (- end-block start-block))
+        (thresholds (unwrap-panic (contract-call? 'SP2ZNGJ85ENDY6QRHQ5P2D4FXKGZWCKTB2T0Z55KS.meme-engine-manager get-thresholds))))
+        (if (>= block-delta (get threshold-39-point thresholds)) (calculate-balance-integral-39 address start-block end-block)
+        (if (>= block-delta (get threshold-19-point thresholds)) (calculate-balance-integral-19 address start-block end-block)
+        (if (>= block-delta (get threshold-9-point thresholds)) (calculate-balance-integral-9 address start-block end-block)
+        (if (>= block-delta (get threshold-5-point thresholds)) (calculate-balance-integral-5 address start-block end-block)
+        (calculate-balance-integral-2 address start-block end-block)))))))
 
 ;; --- Quote Functions ---
 
