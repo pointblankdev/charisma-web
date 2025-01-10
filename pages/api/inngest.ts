b062908bc00674332fcaece745bcca6d61ae630a
Ross Ragsdale <rossragsdale@gmail.com>
2025-01-09 20:23:31 -0600

    :art: Update

--- pages/api/inngest.ts
+++ pages/api/inngest.ts
@@ -4,10 +4,19 @@ import { serve } from "inngest/next";
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "dexterity" });
 
+export const helloWorld = inngest.createFunction(
+    { id: "hello-world" },
+    { event: "test/hello.world" },
+    async ({ event, step }) => {
+        await step.sleep("wait-a-moment", "1s");
+        return { message: `Hello ${event.data.email}!` };
+    },
+);
+
 // Create an API that serves zero functions
 export default serve({
     client: inngest,
     functions: [
-        /* your functions will be passed here later! */
+        helloWorld, // <-- This is where you'll always add your new functions
     ],
 });
