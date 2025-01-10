eaf6ed9eb2630f6ad3e2e73de848a135dca0c9c0
Ross Ragsdale <rossragsdale@gmail.com>
2025-01-10 10:01:15 -0600

    :art: Update cron

--- pages/api/v0/dexterity/balancer.ts
+++ pages/api/v0/dexterity/balancer.ts
@@ -1,5 +1,16 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { inngest } from '@lib/ingest';
+import { ContractId } from 'dexterity-sdk';
+import { Dexterity } from 'dexterity-sdk';
+import PricesService from '@lib/server/prices/prices-service';
+
+const blacklist = [
+    'SP39859AD7RQ6NYK00EJ8HN1DWE40C576FBDGHPA0.chdollar',
+    'SP39859AD7RQ6NYK00EJ8HN1DWE40C576FBDGHPA0.dmg-runes',
+    'SP39859AD7RQ6NYK00EJ8HN1DWE40C576FBDGHPA0.uahdmg',
+    'SP39859AD7RQ6NYK00EJ8HN1DWE40C576FBDGHPA0.dmg-lp-token',
+    'SP39859AD7RQ6NYK00EJ8HN1DWE40C576FBDGHPA0.stx-lp-token',
+] as ContractId[];
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 
@@ -10,22 +21,27 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     try {
         console.log('Dexterity Balancer Cron Job Running')
+        // Initialize Dexterity SDK
+        await Dexterity.configure({
+            apiKeyRotation: 'loop',
+            parallelRequests: 10,
+            maxHops: 4
+        })
 
-        await inngest.send({
-            name: "balance",
-            data: {},
-        });
+        const prices = await PricesService.getInstance().getAllTokenPrices();
+        await Dexterity.discover({ blacklist })
 
-        await inngest.send({
-            name: "swap",
-            data: {
-                from: ".stx",
-                to: 'SP2ZNGJ85ENDY6QRHQ5P2D4FXKGZWCKTB2T0Z55KS.charisma-token',
-                amount: 100000
-            },
-        });
-
-        return res.status(200).json({});
+        const tokens = Dexterity.getTokens()
+        const txs = []
+        for (const token of tokens) {
+            if (Dexterity.getVaultsForToken(token.contractId).size <= 1) continue
+            const amount = Math.floor(10 ** token.decimals / prices[token.contractId])
+            const quote = await Dexterity.getQuote(token.contractId, token.contractId, amount)
+            if (!quote.route.hops.length) continue
+            const tx = await Dexterity.router.executeSwap(quote.route, amount, { fee: 1000 }) as any
+            txs.push(tx)
+        }
+        return res.status(200).json({ txs });
 
     } catch (error) {
         console.error('Arbitrage scan error:', error);
