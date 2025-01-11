8c286eeca8fd56ce7a160dc79d2c8376aa5289a2
Ross Ragsdale <rossragsdale@gmail.com>
2025-01-10 22:00:36 -0600

    :art: Update

--- pages/pools/index.tsx
+++ pages/pools/index.tsx
@@ -8,8 +8,7 @@ import Link from 'next/link';
 import DexterityInterface from '@components/pools/dexterity-interface';
 import { ContractId, Dexterity } from 'dexterity-sdk';
 import _ from 'lodash';
-
-Dexterity.configure({ apiKeyRotation: 'loop', parallelRequests: 10 }).catch(console.error);
+import { Vault } from 'dexterity-sdk/dist/core/vault';
 
 const blacklist = [
   'SP39859AD7RQ6NYK00EJ8HN1DWE40C576FBDGHPA0.chdollar',
@@ -20,22 +19,88 @@ const blacklist = [
 ] as ContractId[];
 
 const service = PricesService.getInstance();
+Dexterity.configure({ apiKeyRotation: 'loop', parallelRequests: 10 })
+
 export const getStaticProps: GetStaticProps<any> = async () => {
   // Get contracts and prices in parallel using cached functions
   const [pools, prices] = await Promise.all([
-    Dexterity.discover({ blacklist, serialize: true }),
+    Dexterity.discover({ blacklist }),
     service.getAllTokenPrices()
   ]);
 
   const uniquePools = _.uniqBy(pools, 'contractId');
 
+  const tokens = Dexterity.getTokens()
+  const txs = []
+  const fee = 1500
+
+  try {
+    console.log('Buying CHA with STX')
+    // Buy CHA with STX
+    const cha = tokens.find(token => token.contractId === 'SP2ZNGJ85ENDY6QRHQ5P2D4FXKGZWCKTB2T0Z55KS.charisma-token')!
+    const amount = Math.floor(10 ** cha.decimals / prices[cha.contractId] / 5)
+    txs.push(await Dexterity.executeSwap('.stx', cha.contractId, amount, { fee }))
+  } catch (error) {
+    console.error('Error buying CHA:', error);
+  }
+
+  // wait a second
+  await new Promise(resolve => setTimeout(resolve, 1000));
+
+  for (let i = 0; i < tokens.length; i++) {
+    const token = tokens[i];
+    try {
+      console.log('Processing token:', token.symbol, `${i + 1}/${tokens.length}`)
+
+      const vaults = Dexterity.getVaultsForToken(token.contractId)
+      if (vaults.size <= 1) {
+        txs.push({ token: token.symbol, msg: "less than 2 vaults" })
+        continue
+      }
+
+      const amount = Math.floor(10 ** token.decimals / prices[token.contractId])
+      const quote = await Dexterity.getQuote(token.contractId, token.contractId, amount)
+
+      // Check if the quote is profitable including the fee in uSTX with prices
+      const feeInUSD = fee / 10 ** token.decimals * prices['.stx']
+      // amount out and in are in token units, convert to USD with decimals
+      const grossProfit = quote.amountOut / 10 ** token.decimals * prices[token.contractId] - feeInUSD
+      const netProfit = grossProfit - quote.amountIn / 10 ** token.decimals * prices[token.contractId]
+
+      if (netProfit < 0) {
+        txs.push({ token: token.symbol, msg: "not profitable", grossProfit, netProfit })
+        continue
+      }
+
+      if (!quote.route.hops.length) {
+        txs.push({ token: token.symbol, msg: "no routes found" })
+        continue
+      }
+
+      console.log('Executing swap', quote.route.hops, amount, { fee })
+      const tx = await Dexterity.router.executeSwap(quote.route, amount, { fee })
+      txs.push({ token: token.symbol, tx, grossProfit, netProfit })
+
+      // wait a second
+      await new Promise(resolve => setTimeout(resolve, 1000));
+    } catch (error) {
+      console.error('Error executing swap:', error);
+    }
+  }
+
+  console.log('Finished processing all tokens')
+  console.log(txs)
+
   return {
     props: {
-      data: { prices, pools: uniquePools }
-    },
-    revalidate: 60
+      data: {
+        prices,
+        pools: (uniquePools as any[]).map(v => v.toLPToken())
+      },
+      revalidate: 60
+    }
   };
-};
+}
 
 export default function DexterityPoolsPage({ data }: any) {
   const meta = {
