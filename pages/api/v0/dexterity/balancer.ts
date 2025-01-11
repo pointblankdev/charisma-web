b2971330c611d2519efcc84d3832c9b911e05939
Ross Ragsdale <rossragsdale@gmail.com>
2025-01-10 20:13:32 -0600

    :art: Update

--- pages/api/v0/dexterity/balancer.ts
+++ pages/api/v0/dexterity/balancer.ts
@@ -96,6 +96,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             }
         }
 
+        console.log('Finished processing all tokens')
+        console.log(txs)
+
         return res.status(200).json({ txs });
 
     } catch (error) {
