8feb132c007d595b116955a638b26068660ad338
Ross Ragsdale <rossragsdale@gmail.com>
2025-01-22 03:47:33 -0600

    :art: Update

---
+++ pages/api/v0/dexterity/farmer.ts
@@ -0,0 +1,42 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { Dexterity } from 'dexterity-sdk';
+import { makeContractCall, PostConditionMode } from '@stacks/transactions';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    res.writeHead(200, {
+        'Cache-Control': 'no-cache',
+        'Connection': 'keep-alive',
+    });
+
+    // Verify the request is from Vercel Cron
+    // if (process.env.VERCEL_ENV === 'production' && !req.headers['x-vercel-cron']) {
+    //     return res.status(401).json({ error: 'Unauthorized' });
+    // }
+
+    try {
+        console.log('Dexterity Farmer Cron Job Running')
+
+        const tx = await makeContractCall({
+            senderKey: Dexterity.config.privateKey,
+            network: Dexterity.config.network,
+            contractAddress: 'SPGYCP878RYFVT03ZT8TWGPKNYTSQB1578VVXHGE',
+            contractName: 'farmer',
+            functionName: 'execute-1-swap',
+            functionArgs: [],
+            postConditionMode: PostConditionMode.Allow,
+            postConditions: [],
+        });
+
+        console.log('Farmer transaction executed:', tx)
+
+        return res.status(200).json({ tx });
+
+    } catch (error) {
+        console.error('Farmer execution error:', error);
+
+        return res.status(500).json({
+            status: 'error',
+            error: 'Internal server error during task'
+        });
+    }
+} 
