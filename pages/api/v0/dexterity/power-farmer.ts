d3fc2496ab606cfc9b3f465ea7e339aabf38aa11
Ross Ragsdale <rossragsdale@gmail.com>
2025-01-23 04:25:34 -0600

    :wrench: Add new farmer cron

---
+++ pages/api/v0/dexterity/power-farmer.ts
@@ -0,0 +1,50 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { broadcastTransaction, makeContractCall, PostConditionMode, principalCV, stringAsciiCV } from '@stacks/transactions';
+import { generateWallet } from '@stacks/wallet-sdk';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    res.writeHead(200, {
+        'Cache-Control': 'no-cache',
+        'Connection': 'keep-alive',
+    });
+
+    // Verify the request is from Vercel Cron
+    // if (process.env.VERCEL_ENV === 'production' && !req.headers['x-vercel-cron']) {
+    //     return res.status(401).json({ error: 'Unauthorized' });
+    // }
+
+    try {
+        console.log('Dexterity Power-Farmer Cron Job Running')
+
+        const wallet = await generateWallet({
+            secretKey: process.env.SEED_PHRASE_2!,
+            password: "",
+        });
+
+        const tx = await makeContractCall({
+            senderKey: wallet.accounts[0].stxPrivateKey,
+            network: 'mainnet',
+            contractAddress: 'SPGYCP878RYFVT03ZT8TWGPKNYTSQB1578VVXHGE',
+            contractName: 'powerful-farmer',
+            functionName: 'execute-both',
+            functionArgs: [],
+            postConditionMode: PostConditionMode.Allow,
+            postConditions: [],
+            fee: 1000,
+        });
+
+        console.log('Farmer transaction created:', tx)
+
+        const broadcastTx = await broadcastTransaction({ transaction: tx });
+
+        return res.status(200).json({ tx: broadcastTx });
+
+    } catch (error) {
+        console.error('Farmer execution error:', error);
+
+        return res.status(500).json({
+            status: 'error',
+            error: 'Internal server error during task'
+        });
+    }
+} 
