fb1f2d24ae6c3473e9719cee8be26bdc4e71ce48
Ross Ragsdale <ross@pointblankdev.com>
2025-02-25 04:30:06 -0600

    :ambulance: Fix price feed

--- lib/kraxel/index.ts
+++ lib/kraxel/index.ts
@@ -24,9 +24,8 @@ async function getAllTokenPrices() {
       }
 
       const { data } = await response.json();
-      const cmcPriceData = await cmc.getQuotes({ symbol: ['STX'] });
-      data.prices['.stx'] = cmcPriceData.data['STX'].quote.USD.price;
-      data.prices['SP2C2YFP12AJZB4MABJBAJ55XECVS7E4PMMZ89YZR.usda-token'] = 0.967;
+      data.prices['.stx'] = data.prices['STX']
+      // data.prices['SP2C2YFP12AJZB4MABJBAJ55XECVS7E4PMMZ89YZR.usda-token'] = 0.967;
 
       return data.prices;
     } catch (error) {
