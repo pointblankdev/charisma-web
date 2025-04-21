b8189d54ca1c8501a2f00b84649ec02f3dfd5e9d
Ross Ragsdale <ross@pointblankdev.com>
2025-04-21 06:38:48 -0500

    :art: Update

--- pages/api/v0/dexterity/investor.ts
+++ pages/api/v0/dexterity/investor.ts
@@ -44,7 +44,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             console.log('Buying CHA with STX')
             // Buy CHA with STX
             const cha = tokens.find(token => token.contractId === 'SP2ZNGJ85ENDY6QRHQ5P2D4FXKGZWCKTB2T0Z55KS.charisma-token')!
-            const amount = 10
+            const amount = 1
             txs.push(await Dexterity.executeSwap('.stx', cha.contractId, amount, { fee, disablePostConditions: false }))
         } catch (error) {
             console.error('Error buying CHA:', error);
